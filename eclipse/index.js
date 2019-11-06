5477eda8eba1905385129f262f040acf2aaa4401
nclslbrn <nclslbrn@gmail.com>
2019-11-06 21:51:35 +0100

    add info box

--- eclipse/index.js
+++ eclipse/index.js
@@ -1,6 +1,7 @@
 'use strict'
 import style from '../src/sass/project.scss'
 import sketch from './eclipse'
+import show_hide_info from '../src/js/show-hide-info'
 
 const containerElement = document.body
 const loader = document.getElementById('loading')
@@ -9,10 +10,12 @@ const P5 = new p5(sketch, containerElement)
 document.body.removeChild(loader)
 
 var resizeTimeout;
-window.addEventListener('resize', function(event) {
+window.addEventListener('resize', function (event) {
     clearTimeout(resizeTimeout);
-    resizeTimeout = setTimeout(function() {
+    resizeTimeout = setTimeout(function () {
         containerElement.removeChild(containerElement.getElementsByClassName('p5Canvas')[0])
         let P5 = new p5(sketch, containerElement)
     }, 500);
 });
+
+show_hide_info();
