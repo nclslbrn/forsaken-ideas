2ee6d523f0a32e68a72b9f46cc3f5bd39289a569
Nicolas Lebrun <nclslbrn@gmail.com>
2019-11-13 09:39:57 +0100

    it's done: unwanted straight line are gone

--- clifford-attractor/clifford-attractor.js
+++ clifford-attractor/clifford-attractor.js
@@ -1,12 +1,12 @@
-import {
-    newExpression
-} from "babel-types"
+import { newExpression } from "babel-types"
 
-const sketch = (p5) => {
-
+const sketch = p5 => {
     const res = 5
 
-    let a, b, c, d = 0
+    let a,
+        b,
+        c,
+        d = 0
 
     const initPoints = []
     let points = []
@@ -22,59 +22,52 @@ const sketch = (p5) => {
 
         for (let x = 0; x < width; x += res) {
             initPoints.push({
-                "x": x,
-                "y": 0,
-                "vx": 0,
-                "vy": 0
+                x: x,
+                y: 0,
+                vx: 0,
+                vy: 0
             })
             pointsHistory.push([])
         }
         sketch.init()
     }
     p5.draw = () => {
-
         for (let p = 0; p < points.length; p++) {
-
             const angle = cliffordAttractor(points[p].x, points[p].y)
 
             points[p].vx += Math.cos(angle) * 0.3
             points[p].vy += Math.sin(angle) * 0.3
 
-            p5.line(points[p].x, points[p].y, points[p].x + points[p].vx, points[p].y + points[p].vy)
-
-
-            if (
-                points[p].x + points[p].vx > width - 1 ||
-                points[p].y + points[p].vy > height - 1 ||
-                points[p].x + points[p].vx < 1 ||
-                points[p].y + points[p].vy < 1
-            ) {
-                // create a new entry on points history and a new ref
-
-                newLineCrossRef[p] = pointsHistory.length
-                pointsHistory[pointsHistory.length] = []
-            }
-
+            p5.line(
+                points[p].x,
+                points[p].y,
+                points[p].x + points[p].vx,
+                points[p].y + points[p].vy
+            )
             points[p].x += points[p].vx
             points[p].y += points[p].vy
 
-            if (typeof(newLineCrossRef[p]) != 'undefined') {
-
-                pointsHistory[newLineCrossRef[p]].push({
-                    'x': points[p].x,
-                    'y': points[p].y
-                })
-
-
+            if (
+                points[p].x + points[p].vx > width ||
+                points[p].y + points[p].vy > height ||
+                points[p].x + points[p].vx < 0 ||
+                points[p].y + points[p].vy < 0
+            ) {
+                newLineCrossRef[p] = pointsHistory.length
+                pointsHistory[pointsHistory.length] = []
             } else {
-
-                pointsHistory[p].push({
-                    'x': points[p].x,
-                    'y': points[p].y
-                })
-
+                if (typeof newLineCrossRef[p] != "undefined") {
+                    pointsHistory[newLineCrossRef[p]].push({
+                        x: points[p].x,
+                        y: points[p].y
+                    })
+                } else {
+                    pointsHistory[p].push({
+                        x: points[p].x,
+                        y: points[p].y
+                    })
+                }
             }
-
 
             points[p].vx *= 0.99
             points[p].vy *= 0.99
@@ -83,13 +76,9 @@ const sketch = (p5) => {
             if (points[p].y > height) points[p].y = 0
             if (points[p].x < 0) points[p].x = width - 0
             if (points[p].y < 0) points[p].y = height - 0
-
-
-
         }
     }
     sketch.init = () => {
-
         a = Math.random() * 4 - 2
         b = Math.random() * 4 - 2
         c = Math.random() * 4 - 2
@@ -100,13 +89,12 @@ const sketch = (p5) => {
         p5.background(255, 250, 245)
     }
     sketch.getSketchProperties = () => {
-
         p5.noLoop()
         console.log(newLineCrossRef)
         return {
-            'points': pointsHistory,
-            'width': width,
-            'height': height
+            points: pointsHistory,
+            width: width,
+            height: height
         }
     }
 
@@ -118,7 +106,6 @@ const sketch = (p5) => {
         const y1 = Math.sin(b * x) + d * Math.cos(b * y)
 
         return Math.atan2(y1 - y, x1 - x)
-
     }
 }
 
--- clifford-attractor/index.js
+++ clifford-attractor/index.js
@@ -1,44 +1,51 @@
-'use strict'
-import style from '../src/sass/project.scss'
-import fullCanvas from '../src/sass/frame-canvas.scss'
-import sketch from './clifford-attractor'
-import computeSVG from './computeSVG'
-import exportSVG from '../tools/exportSVG'
-import infobox from '../src/js/infobox'
-import handleAction from '../src/js/handle-action'
+"use strict"
+import style from "../src/sass/project.scss"
+import fullCanvas from "../src/sass/frame-canvas.scss"
+import sketch from "./clifford-attractor"
+import computeSVG from "./computeSVG"
+import exportSVG from "../tools/exportSVG"
+import infobox from "../src/js/infobox"
+import handleAction from "../src/js/handle-action"
 
 const containerElement = document.body
-const loader = document.getElementById('loading')
+const loader = document.getElementById("loading")
 
 const P5 = new p5(sketch, containerElement)
 document.body.removeChild(loader)
 
-var resizeTimeout;
-window.addEventListener('resize', function(event) {
-    clearTimeout(resizeTimeout);
+var resizeTimeout
+window.addEventListener("resize", function(event) {
+    clearTimeout(resizeTimeout)
     resizeTimeout = setTimeout(function() {
-        containerElement.removeChild(containerElement.getElementsByClassName('p5Canvas')[0])
+        containerElement.removeChild(
+            containerElement.getElementsByClassName("p5Canvas")[0]
+        )
         let P5 = new p5(sketch, containerElement)
     }, 500)
 })
 
-
 const downloadSVG = () => {
-
     // if (window.confirm('Would you like to download the actual sketch as SVG file ?')) {
 
-    const svgContainerId = 'svg-clipboard'
-    const {
-        points,
-        width,
-        height
-    } = sketch.getSketchProperties()
-    const date = new Date;
-    const filename = 'Clifford-Attractor.' + date.getFullYear() + '-' + date.getMonth() + '-' +
-        date.getDay() + '_' + date.getHours() + '.' + date.getMinutes() + '.' +
-        date.getSeconds() + '--Nicolas_Lebrun.svg'
+    const svgContainerId = "svg-clipboard"
+    const { points, width, height } = sketch.getSketchProperties()
+    const date = new Date()
+    const filename =
+        "Clifford-Attractor." +
+        date.getFullYear() +
+        "-" +
+        date.getMonth() +
+        "-" +
+        date.getDay() +
+        "_" +
+        date.getHours() +
+        "." +
+        date.getMinutes() +
+        "." +
+        date.getSeconds() +
+        "--Nicolas_Lebrun.svg"
 
-    computeSVG(points, 'rgba(0,0,0,0.1)', svgContainerId, width, height)
+    computeSVG(points, "rgba(0,0,0,0.1)", svgContainerId, width, height)
     //exportSVG(svgContainerId, filename)
 }
 
