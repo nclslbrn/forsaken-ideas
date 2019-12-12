64701b9c1b3fd96b4f54ab65ac91754d6d1eaa02
Nicolas Lebrun <nlebrun@y50.local>
2019-12-12 22:39:01 +0100

    draw DLA with lnes

--- diffusion-limited-aggregation/diffusion-limited-aggregation.js
+++ diffusion-limited-aggregation/diffusion-limited-aggregation.js
@@ -1,5 +1,4 @@
 import { newExpression } from "babel-types"
-// walker(x, y, stop, color)
 import Walker from "./walker"
 
 const sketch = p5 => {
@@ -15,35 +14,39 @@ const sketch = p5 => {
         }
     }
     const walkerNum = 5000
+    const walkerSpeed = 64
     let walkers = []
     let tree = []
-    const margin = sketchSize /24
+    let lines = []
+    const margin = sketchSize / 24
 
     p5.setup = () => {
         p5.createCanvas(sketchSize, sketchSize)
         p5.fill(0)
-        tree[0] = new Walker(sketchSize / 2, sketchSize / 2, true, 0)
-
-        for (let w = 0; w < walkerNum; w++) {
-            walkers[w] = new Walker(randPos().x, randPos().y, false)
-        }
+        p5.strokeWeight(0.5)
+        init()
     }
     p5.draw = () => {
         p5.background(255)
 
         for (let w = 0; w < walkerNum; w++) {
             if (walkers[w] != undefined) {
-                for (let move = 0; move < 6; move++) {
+                //for (let move = 0; move < 6; move++) {
                     walkers[w].walk()
-                }
+                //}
             }
 
-            for (let branch = 0; branch < tree.length; branch++) {
+            for (let t = 0; t < tree.length; t++) {
                 if (
-                    p5.sq(walkers[w].x - tree[branch].x) +
-                        p5.sq(walkers[w].y - tree[branch].y) <=
-                    6
+                    p5.sq(walkers[w].x - tree[t].x) +
+                    p5.sq(walkers[w].y - tree[t].y) <=
+                    walkerSpeed
                 ) {
+                    lines.push({
+                        x1: walkers[w].x, y1: walkers[w].y,
+                        x2: tree[t].x, y2: tree[t].y
+                    })
+                   
                     walkers[w].stop = true
                     p5.append(tree, walkers[w])
                     walkers.splice(w, 1)
@@ -51,33 +54,47 @@ const sketch = p5 => {
                         randPos().x,
                         randPos().y,
                         false,
-                        0
+                        0,
+                        walkerSpeed
                     )
+
                 }
 
-                //p5.fill(255, 0, 0, 50)
-                //p5.ellipse(walkers[w].x, walkers[w].y, 5)
             }
         }
-        for (let branch = 0; branch < tree.length; branch++) {
+        for (let i = 0; i < lines.length; i++) {
 
-            // const col = p5.map(branch, 0, tree.length, 0, 255)
+            p5.line(lines[i].x1, lines[i].y1, lines[i].x2, lines[i].y2);
 
-            //p5.fill(col)
-            
-            p5.ellipse(tree[branch].x, tree[branch].y, 2)
-            
-            if( tree[branch].x < margin ||
-                tree[branch].y < margin ||
-                tree[branch].x >sketchSize-margin ||
-                tree[branch].y > sketchSize-margin
+            if( lines[i].x2 < margin ||
+                lines[i].y2 < margin ||
+                lines[i].x2 > sketchSize-margin ||
+                lines[i].y2 > sketchSize-margin
             ) {
                 p5.noLoop()
             }
         }
     }
 
-    sketch.init = () => {}
+    sketch.init = () => {
+        walkers = []
+        tree = []
+        lines = []
+        p5.loop()
+        p5.background(255)
+        tree[0] = new Walker(sketchSize / 2, sketchSize / 2, true, 0, walkerSpeed)
+        for (let w = 0; w < walkerNum; w++) {
+            walkers[w] = new Walker(randPos().x, randPos().y, false, 0, walkerSpeed)
+        }
+        p5.redraw()
+    }
+    sketch.exportPNG = () => {
+        const date = new Date;
+        const filename = 'Diffusion-limited-aggregation.' + date.getFullYear() + '-' + date.getMonth() + '-' +
+            date.getDay() + '_' + date.getHours() + '.' + date.getMinutes() + '.' +
+            date.getSeconds() + '--copyright_Nicolas_Lebrun_CC-by-3.0'
+        p5.saveCanvas(cacheCanvas, filename, 'png')
+    }
 }
 
 
