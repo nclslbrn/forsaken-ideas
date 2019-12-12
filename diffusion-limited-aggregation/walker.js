dca21d541ffccd8f0fc4681b3bd753b1b3634531
Nicolas Lebrun <nlebrun@y50.local>
2019-12-12 22:40:31 +0100

    added speed property to walker class

--- diffusion-limited-aggregation/walker.js
+++ diffusion-limited-aggregation/walker.js
@@ -1,25 +1,26 @@
 export default class Walker {
-    constructor(x, y, stop, colour) {
+    constructor(x, y, stop, colour, speed) {
         this.x = x
         this.y = y
         this.stop = stop
         this.colour = colour
+        this.speed = speed
     }
 
     walk() {
         if (!this.stop) {
             switch (Math.floor(Math.random() * 4)) {
                 case 0:
-                    this.x++
+                    this.x += this.speed
                     break
                 case 1:
-                    this.y--
+                    this.y -= this.speed
                     break
                 case 2:
-                    this.y++
+                    this.y += this.speed
                     break
                 case 3:
-                    this.x--
+                    this.x -= this.speed
                     break
                 default:
                     console.log("Unexpected value")
