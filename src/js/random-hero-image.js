46de7089848e61565247e729fc4dbbdd9af37324
nclslbrn <nl@artemg.com>
2020-08-20 17:16:46 +0200

    add new cover and add size param for each cover

--- src/js/random-hero-image.js
+++ src/js/random-hero-image.js
@@ -1,30 +1,64 @@
 const randomHeroImage = () => {
     const hero = document.getElementById('hero-image')
     const images = [
-        'Mondrian-City.2019-10-2_14.34.29--copyright_Nicolas_Lebrun_CC-by-3.0.svg',
-        'Vector-field.2020-7-0_14.57.46--copyright_Nicolas_Lebrun_CC-by-3.0.png',
-        'Vector-field.2020-7-0_14.54.9--copyright_Nicolas_Lebrun_CC-by-3.0.png',
-        'Vector-field.2020-7-0_15.6.39--copyright_Nicolas_Lebrun_CC-by-3.0.png',
-        'Clifford-Attractor.2020-7-0_16.24.57--Nicolas_Lebrun.svg',
-        'Clifford-Attractor.2020-7-0_16.26.37--Nicolas_Lebrun.svg',
-        'Clifford-Attractor.2020-7-0_16.26.28--Nicolas_Lebrun.svg',
-        'Clifford-Attractor.2020-7-0_16.26.18--Nicolas_Lebrun.svg'
+        {
+            url:
+                'Mondrian-City.2019-10-2_14.34.29--copyright_Nicolas_Lebrun_CC-by-3.0.svg',
+            style: '50%/cover'
+        },
+        {
+            url:
+                'Vector-field.2020-7-0_14.57.46--copyright_Nicolas_Lebrun_CC-by-3.0.png',
+            style: '50%/contain'
+        },
+        {
+            url:
+                'Vector-field.2020-7-0_14.54.9--copyright_Nicolas_Lebrun_CC-by-3.0.png',
+            style: '50%/contain'
+        },
+        {
+            url:
+                'Vector-field.2020-7-0_15.6.39--copyright_Nicolas_Lebrun_CC-by-3.0.png',
+            style: '50%/contain'
+        },
+        {
+            url: 'Clifford-Attractor.2020-7-0_16.24.57--Nicolas_Lebrun.svg',
+            style: '50%/cover'
+        },
+        {
+            url: 'Clifford-Attractor.2020-7-0_16.26.37--Nicolas_Lebrun.svg',
+            style: '50%/cover'
+        },
+        {
+            url: 'Clifford-Attractor.2020-7-0_16.26.28--Nicolas_Lebrun.svg',
+            style: '50%/cover'
+        },
+        {
+            url: 'Clifford-Attractor.2020-7-0_16.26.18--Nicolas_Lebrun.svg',
+            style: '50%/cover'
+        },
+        {
+            url:
+                'Noise-landscape.2020-7-3_11.7.44--copyright_Nicolas_Lebrun_CC-by-3.0.png',
+            style: '50%/cover'
+        },
+        {
+            url:
+                'Noise-landscape.2020-7-3_11.10.23--copyright_Nicolas_Lebrun_CC-by-3.0.png',
+            style: '50%/cover'
+        },
+        {
+            url:
+                'Moving-light.2020-7-4_16.26.5--copyright_Nicolas_Lebrun_CC-by-3.00.jpg',
+            style: '50%/cover'
+        }
     ]
 
     if (hero && images) {
         const imgId = Math.floor(Math.random(1) * images.length)
-        const ext = images[imgId].split('.').pop()
-        let style
+        const style = `background: ${images[imgId].style} url(img/${images[imgId].url});`
 
-        if (ext == 'png' || ext == 'jpg' || ext == 'gif') {
-            style = `background: 50%/contain url(img/${images[imgId]});`
-        } else if (ext == 'svg') {
-            style = `background: 50%/cover url(img/${images[imgId]});`
-        }
-
-        if (style !== null) {
-            hero.setAttribute('style', style)
-        }
+        hero.setAttribute('style', style)
     }
 }
 
