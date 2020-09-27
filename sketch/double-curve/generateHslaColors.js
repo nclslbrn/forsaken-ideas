bfaf34e932de96f01e4c1422a140a0369f71ec41
nclslbrn <nl@artemg.com>
2020-09-27 16:55:02 +0200

    add a function to generate color

---
+++ sketch/double-curve/generateHslaColors.js
@@ -0,0 +1,27 @@
+/**
+ * Generate multiple colors
+ *
+ * @param {int} saturation in percent the saturation of the returned colors
+ * @param {int} lightness in percent the brightness value of generated colors
+ * @param {int} alpha in percent the alpha value of returned colors
+ * @param {int} amount how many color you want
+ */
+const generateHslaColors = (
+    saturation = 75,
+    lightness = 75,
+    alpha = 100,
+    amount
+) => {
+    const colors = []
+    const hueBegin = Math.floor(Math.random() * 360)
+    const hueDelta = Math.trunc(360 / amount)
+
+    for (let i = 0; i < amount; i++) {
+        let hue = (hueBegin + i * hueDelta) % 360
+        colors.push([hue, saturation, lightness, alpha])
+    }
+
+    return colors
+}
+
+export { generateHslaColors }
