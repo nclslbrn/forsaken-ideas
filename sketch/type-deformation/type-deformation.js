1db80ab5a47a5042856099cca3f4a51716fb416d
nclslbrn <nl@artemg.com>
2020-10-23 17:36:32 +0200

    split word by letter

--- sketch/type-deformation/type-deformation.js
+++ sketch/type-deformation/type-deformation.js
@@ -6,27 +6,41 @@ let canvas
 const sketch = (p5) => {
     const numFrame = 600
     const initScale = 0.001
-    const particlesCount = 120
+    const particlesCount = 60
     const backgroundCol = 8
-    let font, word, points, bounds, scale, margin, particles
+    let font, word, lettersPoints, bounds, scale, margin, particles
 
     const noiseScale = (x, y, z, scale) => {
         return scale * p5.map(p5.noise(x, y, z), 0, 1, -1, 1)
     }
 
     sketch.init = () => {
-        word = eWords[p5.floor(p5.random() * eWords.length)]
-        word.toUpperCase()
-        // TODO: split word by letter and store it into nested array
-        points = font.textToPoints(word, 0, 0, 15, {
-            sampleFactor: 15,
-            simplifyThreshold: 0.1
-        })
+        word = eWords[p5.floor(p5.random() * eWords.length)] //.toUpperCase()
+        lettersPoints = []
+        let _x = 0
+        for (let i = 0; i < word.length; i++) {
+            const points = font.textToPoints(word.charAt(i), 0, 0, 15, {
+                sampleFactor: 20,
+                simplifyThreshold: 0.1
+            })
+
+            for (let j = 0; j < points.length; j++) {
+                points[j].x += _x
+            }
+            lettersPoints[i] = points
+            const letterBounds = font.textBounds(word.charAt(i), 0, 0, 15)
+            _x += letterBounds.w
+        }
+
         bounds = font.textBounds(` ${word} `, 0, 0, 15)
         scale = initScale
         particles = []
         for (let i = 0; i < particlesCount; i++) {
-            const point = points[Math.floor(Math.random() * points.length)]
+            const letter = Math.floor(Math.random() * lettersPoints.length)
+            const point =
+                lettersPoints[letter][
+                    Math.floor(Math.random() * lettersPoints[letter].length)
+                ]
             particles.push({ x: point.x, y: point.y })
         }
         p5.background(backgroundCol)
@@ -50,40 +64,57 @@ const sketch = (p5) => {
         if (p5.frameCount % numFrame !== 0) {
             const t = (p5.frameCount % numFrame) / numFrame
 
-            p5.push()
-            p5.translate(p5.width / bounds.x, 0)
-            p5.beginShape()
+            for (let i = 0; i < lettersPoints.length; i++) {
+                p5.push()
+                p5.translate(p5.width / bounds.x, 0)
+                p5.beginShape()
 
-            for (let i = 0; i < points.length; i++) {
-                const x = p5.map(
-                    points[i].x,
-                    bounds.x,
-                    bounds.w,
-                    margin,
-                    p5.width - margin
-                )
-                const y = p5.map(
-                    points[i].y,
-                    bounds.y,
-                    bounds.h,
-                    p5.height * 0.27,
-                    p5.height
-                )
+                for (let j = 0; j < lettersPoints[i].length; j++) {
+                    const x = p5.map(
+                        lettersPoints[i][j].x,
+                        bounds.x,
+                        bounds.w,
+                        margin,
+                        p5.width - margin
+                    )
+                    const y = p5.map(
+                        lettersPoints[i][j].y,
+                        bounds.y,
+                        bounds.h,
+                        p5.height * 0.27,
+                        p5.height
+                    )
 
-                p5.vertex(x, y)
-                const n1 = noiseScale(points[i].x, points[i].y, t, 30)
-                const n2 = noiseScale(points[i].x, points[i].y, t, 15)
-                const n3 = noiseScale(points[i].x, points[i].y, t, 10)
+                    p5.vertex(x, y)
+                    const n1 = noiseScale(
+                        lettersPoints[i][j].x,
+                        lettersPoints[i][j].y,
+                        t,
+                        30
+                    )
+                    const n2 = noiseScale(
+                        lettersPoints[i][j].x,
+                        lettersPoints[i][j].y,
+                        t,
+                        15
+                    )
+                    const n3 = noiseScale(
+                        lettersPoints[i][j].x,
+                        lettersPoints[i][j].y,
+                        t,
+                        10
+                    )
 
-                const v = {
-                    x: Math.cos(n1 + n2 + n3),
-                    y: Math.sin(n1 + n2 + n3)
+                    const v = {
+                        x: Math.cos(n1 + n2 + n3),
+                        y: Math.sin(n1 + n2 + n3)
+                    }
+                    lettersPoints[i][j].x += v.x * scale
+                    lettersPoints[i][j].y += v.y * scale
                 }
-                points[i].x += v.x * scale
-                points[i].y += v.y * scale
+                p5.endShape(p5.CLOSE)
+                p5.pop()
             }
-            p5.endShape()
-            p5.pop()
 
             for (let i = 0; i < particles.length; i++) {
                 const x = p5.map(
@@ -109,7 +140,7 @@ const sketch = (p5) => {
                 particles[i].x += v.x * scale
                 particles[i].y += v.y * scale
             }
-            scale += 0.00005
+            scale += 0.00008
             // smooth word transition
             if (t > 0.9) {
                 p5.background(backgroundCol, (t - 0.9) * 2550)
