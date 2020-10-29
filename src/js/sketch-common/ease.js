ffdcfd3ee7429ebb72935660b1f60dd5b23a5734
nclslbrn <nl@artemg.com>
2020-10-29 10:04:27 +0100

    add author comment on ease function

--- src/js/sketch-common/ease.js
+++ src/js/sketch-common/ease.js
@@ -1,3 +1,9 @@
+/**
+ * Ease function adapted from processing
+ * @param {float} p the var (between 0 & 1) to ease
+ * @author beesandbombs https://gist.github.com/beesandbombs
+ */
+
 const ease = (p) => {
     return 3 * p * p - 2 * p * p * p
 }
