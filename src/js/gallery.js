5c9963f9f5d6f61594d78b796fe7d1b65fc278b6
nclslbrn <nl@artemg.com>
2020-10-29 10:01:46 +0100

    adjust side content (homepage)

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,14 +1,6 @@
 import style from '../sass/gallery.scss'
 import makeAcronym from './gallery/random-acronym-extend'
 import randomHeroImage from './gallery/random-hero-image'
-const acronymElement = document.getElementById('acronym')
 
-if (acronymElement != null) {
-    makeAcronym(acronymElement)
-
-    acronymElement.addEventListener('click', function () {
-        makeAcronym(acronymElement)
-    })
-}
-
+makeAcronym()
 randomHeroImage()
--- src/js/gallery/random-acronym-extend.js
+++ src/js/gallery/random-acronym-extend.js
@@ -1,9 +1,10 @@
 import textStock from '../../json/e-m-g-words.json'
 
-const makeAcronym = (acronymElem) => {
-    let randomAcronym = ''
-    const textStockKey = ['e', 'm', 'g']
-    if (acronymElem) {
+const makeAcronym = () => {
+    const acronymElement = document.getElementById('acronym')
+    const insertRandomAcronym = (acronymElem) => {
+        const textStockKey = ['e', 'm', 'g']
+        let randomAcronym = ''
         textStockKey.forEach((letter, index) => {
             if (textStock[letter]) {
                 let randomWord =
@@ -15,8 +16,20 @@ const makeAcronym = (acronymElem) => {
                 if (index !== textStockKey.length - 1) randomAcronym += ' '
             }
         })
-
         acronymElem.innerHTML = randomAcronym
+    }
+    if (acronymElement && textStock) {
+        insertRandomAcronym(acronymElement)
+        const button = document.createElement('button')
+        button.innerHTML =
+            '<span class="screen-reader-text">Random acronym</span>' +
+            '<span class="icon">&nesear;</span>'
+
+        button.classList.add('button')
+        acronymElement.parentNode.appendChild(button)
+        acronymElement.parentNode.addEventListener('click', (event) => {
+            insertRandomAcronym(acronymElement)
+        })
     }
 }
 
