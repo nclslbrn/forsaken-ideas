b1d301216670ee7afae0ddd493ba6229c5bcdb15
nclslbrn <nl@artemg.com>
2020-11-19 15:58:04 +0100

    remove order by date function

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,8 +1,6 @@
 import style from '../sass/gallery.scss'
 import makeAcronym from './gallery/random-acronym-extend'
 import randomHeroImage from './gallery/random-hero-image'
-import projectOrder from './gallery/projectOrder'
 
 makeAcronym()
 randomHeroImage()
-projectOrder()
