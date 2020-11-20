2e356fcbf59df20c2798bd81e3bc8cf7a37f8bb1
nclslbrn <nl@artemg.com>
2020-11-20 17:36:53 +0100

    update the three.js template/boilerplate

--- templates/three-template/index.js
+++ templates/three-template/index.js
@@ -1,18 +1,14 @@
 'use strict'
-import style from '../../src/sass/project.scss'
-import fullCanvas from '../../src/sass/full-canvas.scss'
-import sketch from './three-template'
+import '../../src/sass/project.scss'
+import '../../src/sass/full-canvas.scss'
+import threeTemplate from './three-template'
 import infobox from '../../src/js/sketch-common/infobox'
 import handleAction from '../../src/js/sketch-common/handle-action'
 
-const containerElement = document.body
+const windowFrame = document.getElementById('windowFrame')
 const loader = document.getElementById('loading')
-sketch.launch()
-document.body.removeChild(loader)
 
-/*
-window.init = sketch.init
-window.export_PNG = sketch.exportPNG
-*/
+threeTemplate.launch()
+windowFrame.removeChild(loader)
 window.infobox = infobox
 handleAction()
--- templates/three-template/three-template.js
+++ templates/three-template/three-template.js
@@ -1,95 +1,55 @@
-// https://github.com/mrdoob/three.js/blob/master/examples/webgl_geometry_dynamic.html
+import * as three from 'three'
+let camera, scene, renderer, clock
+let geometry, material, cube
+const windowFrame = document.getElementById('windowFrame')
 
-import * as THREE from 'three'
-import { FirstPersonControls } from 'three/examples/js/controls/FirstPersonControls'
-let camera, controls, scene, renderer
-let mesh, geometry, material, clock
-let worldWidth = 128,
-    worldDepth = 128
-
-const sketch = {
+const threeTemplate = {
     init: () => {
-        camera = new THREE.PerspectiveCamera(
+        camera = new three.PerspectiveCamera(
             60,
             window.innerWidth / window.innerHeight,
             1,
             20000
         )
-        camera.position.y = 200
-
-        clock = new THREE.Clock()
-
-        scene = new THREE.Scene()
-        scene.background = new THREE.Color(0xaaccff)
+        //camera.position.y = 200
+        clock = new three.Clock()
+        scene = new three.Scene()
+        scene.background = new THREE.Color(0x333333)
         scene.fog = new THREE.FogExp2(0xaaccff, 0.0007)
+        renderer = new three.WebGLRenderer({ antialias: true })
 
-        geometry = new THREE.PlaneBufferGeometry(
-            20000,
-            20000,
-            worldWidth - 1,
-            worldDepth - 1
-        )
-        geometry.rotateX(-Math.PI / 2)
-
-        const position = geometry.attributes.position
-        position.usage = THREE.DynamicDrawUsage
-
-        for (var i = 0; i < position.count; i++) {
-            var y = 35 * Math.sin(i / 2)
-            position.setY(i, y)
-        }
-
-        const texture = new THREE.TextureLoader().load('assets/water.jpg')
-        texture.wrapS = texture.wrapT = THREE.RepeatWrapping
-        texture.repeat.set(5, 5)
-
-        material = new THREE.MeshBasicMaterial({
-            color: 0x0044ff,
-            map: texture
-        })
-
-        mesh = new THREE.Mesh(geometry, material)
-        scene.add(mesh)
-
-        renderer = new THREE.WebGLRenderer({ antialias: true })
         renderer.setPixelRatio(window.devicePixelRatio)
         renderer.setSize(window.innerWidth, window.innerHeight)
-        document.body.appendChild(renderer.domElement)
+        windowFrame.appendChild(renderer.domElement)
 
-        controls = new FirstPersonControls(camera, renderer.domElement)
-
-        controls.movementSpeed = 500
-        controls.lookSpeed = 0.1
-
-        window.addEventListener('resize', sketch.onWindowResize, false)
+        geometry = new three.BoxGeometry(0.1, 1, 1)
+        material = new three.MeshBasicMaterial({ color: 0xffffff })
+        cube = new three.Mesh(geometry, material)
+        cube.rotation.x = Math.PI * 0.5
+        cube.rotation.y = Math.PI * -0.5
+        scene.add(cube)
+        camera.position.z = 5
+        window.addEventListener('resize', threeTemplate.onWindowResize, false)
+        threeTemplate.render()
     },
     onWindowResize: () => {
         camera.aspect = window.innerWidth / window.innerHeight
         camera.updateProjectionMatrix()
         renderer.setSize(window.innerWidth, window.innerHeight)
-        controls.handleResize()
     },
     render: () => {
         const delta = clock.getDelta()
         const time = clock.getElapsedTime() * 10
-        const position = geometry.attributes.position
-        for (let i = 0; i < position.count; i++) {
-            const y = 35 * Math.sin(i / 5 + (time + i) / 7)
-            position.setY(i, y)
-        }
-        position.needsUpdate = true
-        controls.update(delta)
+        cube.rotation.z += 0.01
         renderer.render(scene, camera)
     },
     animate: () => {
-        requestAnimationFrame(sketch.animate)
-        if (sketch.render !== undefined) sketch.render()
+        requestAnimationFrame(threeTemplate.animate)
+        if (threeTemplate.render !== undefined) threeTemplate.render()
     },
-
     launch: () => {
-        sketch.init()
-        sketch.animate()
+        threeTemplate.init()
+        threeTemplate.animate()
     }
 }
-
-export default sketch
+export default threeTemplate
