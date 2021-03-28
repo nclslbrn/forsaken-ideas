d96d0ebccf6b40f46b8841316f8342313c93c891
nclslbrn <nl@artemg.com>
2021-03-28 22:11:01 +0200

    change sketch name

--- sketch/curved-checker-board/index.js
+++ sketch/curved-checker-board/index.js
@@ -1,13 +1,13 @@
 'use strict'
 import '../../src/sass/project.scss'
 import '../../src/sass/frame-canvas.scss'
-import sketch from './checker-board-curve'
+import sketch from './curved-checker-board'
 import infobox from '../../src/js/sketch-common/infobox'
 import handleAction from '../../src/js/sketch-common/handle-action'
 
 const windowFrame = document.getElementById('windowFrame')
 const loader = document.getElementById('loading')
-const P5 = new p5(sketch, windowFrame)
+new p5(sketch, windowFrame)
 windowFrame.removeChild(loader)
 
 /*
