cdefc86d8c7992202521dc7afd1e341ef029d355
nclslbrn <nl@artemg.com>
2021-04-29 16:23:14 +0200

    clean

--- sketch/hexagon-pulse/hexagon-pulse.js
+++ sketch/hexagon-pulse/hexagon-pulse.js
@@ -2,43 +2,33 @@
 import ease from '../../src/js/sketch-common/ease'
 
 const sketch = (p5) => {
-    const numFrame = 60
+    const numFrame = 120
     let canvasSize, middle, numRow, step
     const sketchSize = () => {
         const side = Math.min(window.innerWidth, window.innerHeight)
         return side > 800 ? 800 : side * 0.85
     }
     p5.hexagon = (center, radius, angle, side, t) => {
-        const _t = (t < 0.5 ? t : 1 - t) * 2
+        //const _t = (t < 0.5 ? 0.5 + t : 1.5 - t) * 2
+        p5.beginShape()
         for (let theta = 0; theta < Math.PI * 2; theta += Math.PI / side) {
-            p5.beginShape()
-            // p5.vertex(center.x, center.y)
-            p5.vertex(
-                center.x + radius * _t * Math.cos(theta),
-                center.y + radius * _t * Math.sin(theta)
-            )
+            //const r = theta + Math.PI * t
             p5.vertex(
                 center.x + radius * Math.cos(theta + angle),
                 center.y + radius * Math.sin(theta + angle)
             )
-            p5.endShape(p5.CLOSE)
-            /*  p5.line(
-                center.x + radius * t * Math.cos(theta - angle),
-                center.y + radius * t * Math.sin(theta - angle),
-                center.x + radius * Math.cos(theta + angle),
-                center.y + radius * Math.sin(theta + angle)
-            ) */
         }
+        p5.endShape(p5.CLOSE)
     }
     p5.setup = () => {
         canvasSize = sketchSize()
         p5.createCanvas(canvasSize, canvasSize)
-        step = canvasSize / 24
+        step = canvasSize / 50
         numRow = canvasSize / step
         middle = canvasSize / (step * 2)
         p5.stroke(255)
-        p5.fill(255)
-        p5.strokeWeight(2)
+        p5.fill(0)
+        p5.strokeWeight(4)
         //p5.noStroke()
     }
     p5.draw = () => {
@@ -54,22 +44,9 @@ const sketch = (p5) => {
                     x: x * step,
                     y: (x % 2 == 0 ? y : y + 0.5) * step
                 }
-                const size = step * Math.abs(j) * 0.5 //* 1.15
-                const rot =
-                    ease(
-                        ((p5.frameCount + x + y * numRow) % numFrame) /
-                            numFrame,
-                        5
-                    ) *
-                    Math.PI *
-                    (1 / 3.0)
-                p5.hexagon(
-                    p,
-                    size,
-                    Math.PI * 2 * t,
-                    3,
-                    t > 0.5 ? 0.5 + t : 1.5 - t
-                )
+                const size = step * 0.5 * Math.max(Math.abs(j), 0.15)
+                const rot = (Math.PI / 3) * ease(t, 5)
+                p5.hexagon(p, size, rot, 3, t)
             }
         }
     }
