e4c2be2ff50bdf5328b3cfef7e282eb701949ff4
nclslbrn <nl@artemg.com>
2021-06-01 22:13:41 +0200

    add tracing function

--- sketch/grow-from-pattern/grow-from-pattern--vector.js
+++ sketch/grow-from-pattern/grow-from-pattern--vector.js
@@ -19,13 +19,13 @@ const circle = (theta) => {
 // Main sketch object
 const sketch = {
     nIter: 0,
-    iterations: 150,
+    iterations: 5,
     patternNum: [3, 5, 6, 7, 9, 10, 11, 12, 13, 15],
-    width: 5031,
-    height: 3579,
+    width: 1280, //5031,
+    height: 1280, //3579,
     trigoFunc: false,
-    scale: 512,
-    res: 32,
+    scale: 256,
+    res: 64,
     points: [],
     lines: [],
     root: document.getElementById('windowFrame'),
@@ -43,17 +43,19 @@ const sketch = {
         )
         sketch.svg.setAttribute(
             'style',
-            'height: 85vh; width: auto; box-shadow: 0 0.5em 1em rgba(0,0,0,0.1);'
+            'height: 85vh; width: auto; background: #fff; box-shadow: 0 0.5em 1em rgba(0,0,0,0.1);'
         )
         sketch.root.appendChild(sketch.svg)
-        //window.requestAnimationFrame(sketch.update)
+        sketch.reset()
     },
     init_points: () => {
         const g =
             sketch.patternNum[
                 Math.floor(Math.random() * sketch.patternNum.length)
             ]
-        sketch.points = sketch.lines = []
+        sketch.points = []
+        sketch.lines = []
+
         for (let x = 0; x <= sketch.width; x += sketch.scale) {
             for (let y = 0; y <= sketch.height; y += sketch.scale) {
                 if ((x ^ y) % g) {
@@ -71,27 +73,57 @@ const sketch = {
         for (let i = 0; i < sketch.points.length; i++) sketch.lines.push([])
     },
     update: () => {
-        /*    for (let p = 0; p < points.length; p++) {
-            const xx = map(points[p].x, -4, 4, 0, sketch.width)
-            const yy = map(points[p].y, -4, 4, 0, sketch.height)
+        for (let p = 0; p < sketch.points.length; p++) {
+            const xx = map(sketch.points[p].x, -4, 4, 0, sketch.width)
+            const yy = map(sketch.points[p].y, -4, 4, 0, sketch.height)
             const n1 =
                 500 *
-                map(simplex.noise2D(points[p].x, points[p].y), 0, 1, -1, 1)
+                map(
+                    simplex.noise2D(sketch.points[p].x, sketch.points[p].y),
+                    0,
+                    1,
+                    -1,
+                    1
+                )
             const v1 = circle(n1)
             const v2 = funcs[sketch.trigoFunc](v1)
             const v = funcs['sinusoidal'](v2)
 
-            points[p].x += scale * v.x
-            points[p].y += scale * v.y
+            sketch.points[p].x += sketch.scale * v.x
+            sketch.points[p].y += sketch.scale * v.y
+
+            sketch.lines[p].push([xx, yy])
         }
+
         if (sketch.nIter < sketch.iterations) {
             requestAnimationFrame(sketch.update)
-        } */
+            sketch.nIter++
+        } else {
+            sketch.print()
+        }
     },
-    print: () => {},
+    print: () => {
+        while (sketch.svg.firstChild) {
+            sketch.svg.removeChild(sketch.svg.firstChild)
+        }
+        for (let i = 0; i < sketch.lines.length; i++) {
+            const path = document.createElementNS(
+                'http://www.w3.org/2000/svg',
+                'path'
+            )
+            let d = `M ${sketch.lines[i][0][0]} ${sketch.lines[i][0][1]}`
+
+            for (let j = 1; j < sketch.lines[i].length; j++) {
+                d += ` L${sketch.lines[i][j][0]} ${sketch.lines[i][j][1]}`
+            }
+            path.setAttribute('d', d)
+            sketch.svg.appendChild(path)
+        }
+    },
     reset: () => {
         sketch.trigoFunc = randomTrigoFunc()
         sketch.init_points()
+        sketch.update()
     }
 }
 export default sketch
