fd17988ef2f88039ac48760d561e5631a371d827
nclslbrn <nl@artemg.com>
2021-06-03 18:49:14 +0200

    remove negative scaling (inkscape don't like it)

--- sketch/cellular-automata/cellular-automata--ver.vector.js
+++ sketch/cellular-automata/cellular-automata--ver.vector.js
@@ -4,17 +4,14 @@ import {
     getRandomPalette,
     getColorCombination
 } from '../../src/js/sketch-common/stabilo68-colors'
-
-let palette,
-    cellSize,
-    colorsGroups = []
+import MirrorShape from './MirrorShape'
 
 const svgSize = () => {
     // optimized for landscape screen
     // Give it A0 aspect ratio
     return {
-        w: window.innerHeight * 1.414,
-        h: window.innerHeight
+        w: 1122,
+        h: 793
     }
 }
 const rect = (x = 0, y = 0, width = 0, height = 0, color) => {
@@ -23,154 +20,54 @@ const rect = (x = 0, y = 0, width = 0, height = 0, color) => {
     r.setAttribute('y', y)
     r.setAttribute('width', width)
     r.setAttribute('height', height)
-    colorsGroups[color].shapes.push(r)
-    //r.setAttribute('fill', color)
-    //mainSVG.appendChild(r)
+    sketch.groupsByColor[color].appendChild(r)
 }
 const triangle = (p = [], color) => {
     const _x = (v) => {
-        return v * cellSize.w
+        return v * sketch.cellSize.w
     }
     const _y = (v) => {
-        return v * cellSize.h
+        return v * sketch.cellSize.h
     }
     const t = document.createElementNS('http://www.w3.org/2000/svg', 'path')
     t.setAttribute(
         'd',
-        String(
-            'M ' +
-                _x(p[0]) +
-                ',' +
-                _y(p[1]) +
-                'L ' +
-                _x(p[2]) +
-                ',' +
-                _y(p[3]) +
-                'L ' +
-                _x(p[4]) +
-                ',' +
-                _y(p[5]) +
-                'Z'
-        )
+        `M ${_x(p[0])},${_y(p[1])}L ${_x(p[2])},${_y(p[3])}L ${_x(p[4])},${_y(
+            p[5]
+        )}Z`
     )
-    colorsGroups[color].shapes.push(t)
+    sketch.groupsByColor[color].appendChild(t)
 }
 const fillCell = (x, y, color) => {
-    rect(x * cellSize.w, y * cellSize.h, cellSize.w, cellSize.h, color)
-}
-const topLeftTriangle = (x, y, color) => {
-    triangle([x, y, x + 1, y, x, y + 1], color)
-}
-const topRightTriangle = (x, y, color) => {
-    triangle([x, y, x + 1, y, x + 1, y + 1], color)
-}
-const bottomRightTriangle = (x, y, color) => {
-    triangle([x + 1, y, x + 1, y + 1, x, y + 1], color)
-}
-const bottomLeftTriangle = (x, y, color) => {
-    triangle([x + 1, y + 1, x, y + 1, x, y], color)
-}
-const mirrorClone = () => {
-    const mirrors = [
-        {
-            name: 'topLeft',
-            transform: 'scale(1, 1)'
-        },
-        {
-            name: 'topRight',
-            transform: 'scale(-1, 1)'
-        },
-        {
-            name: 'bottomLeft',
-            transform: 'scale(1, -1)'
-        },
-        {
-            name: 'bottomRight',
-            transform: 'scale(-1, -1)'
-        }
-    ]
-    while (mainSVG.firstChild) {
-        mainSVG.removeChild(mainSVG.firstChild)
-    }
-    colorsGroups.forEach((group) => {
-        const domGroup = document.createElementNS(
-            'http://www.w3.org/2000/svg',
-            'g'
+    mirror.allCorners(x, y).forEach((p) => {
+        rect(
+            p[0] * sketch.cellSize.w,
+            p[1] * sketch.cellSize.h,
+            sketch.cellSize.w,
+            sketch.cellSize.h,
+            color
         )
-        domGroup.setAttribute('id', group.colorName)
-        domGroup.setAttribute('fill', group.colorValue)
-        mirrors.forEach((mirror) => {
-            const mirrorGroup = document.createElementNS(
-                'http://www.w3.org/2000/svg',
-                'g'
-            )
-            mirrorGroup.setAttribute('id', group.colorName + '-' + mirror.name)
-            mirrorGroup.setAttribute('transform', mirror.transform)
-            mirrorGroup.setAttribute(
-                'transform-origin',
-                `${svgFrameSize.w / 2}px ${svgFrameSize.h / 2}px`
-            )
-            group.shapes.forEach((node) => {
-                mirrorGroup.appendChild(node.cloneNode(true))
-            })
-            domGroup.appendChild(mirrorGroup)
-        })
-        mainSVG.appendChild(domGroup)
     })
 }
-const print = () => {
-    for (let i = 0; i < colorsGroups.length; i++) colorsGroups[i].shapes = []
-    for (let x = 0; x <= g.cols; x++) {
-        for (let y = 0; y <= g.rows; y++) {
-            const i = x * g.cols + y
-            const colorNum = i % palette.length
-            // top and bootom
-            if (
-                y > 0 &&
-                y < g.rows &&
-                g.value[i - g.rows] &&
-                g.value[i + g.rows]
-            ) {
-                fillCell(x, y, colorNum)
-                fillCell(x, y - 1, colorNum)
-                fillCell(x, y + 1, colorNum)
-            }
-            // left & right
-            if (
-                x > 0 &&
-                x < g.cols &&
-                g.value[i + g.cols] &&
-                g.value[i - g.cols]
-            ) {
-                fillCell(x, y, colorNum)
-                fillCell(x - 1, y, colorNum)
-                fillCell(x + 1, y, colorNum)
-            }
-            if (x > 0 && y > 0 && g.value[i - 1] && g.value[i - g.cols]) {
-                fillCell(x, y, colorNum)
-                bottomRightTriangle(x - 1, y - 1, colorNum)
-            }
-            if (x < g.cols && y > 0 && g.value[i - 1] && g.value[i + g.cols]) {
-                fillCell(x, y, colorNum)
-                bottomLeftTriangle(x + 1, y - 1, colorNum)
-            }
-            if (
-                x < g.cols &&
-                y < g.rows &&
-                g.value[i + 1] &&
-                g.value[i + g.cols]
-            ) {
-                fillCell(x, y, i % palette.length)
-                topLeftTriangle(x + 1, y + 1, colorNum)
-            }
-            if (x > 0 && y < g.rows && g.value[i + 1] && g.value[i - g.cols]) {
-                fillCell(x, y, i % palette.length)
-                topRightTriangle(x - 1, y + 1, colorNum)
-            }
-        }
-    }
-    // clone mirror
-    mirrorClone()
+const topLeftTriangle = (x, y, color) => {
+    mirror.topLeftCorner(x, y).forEach((p) => {
+        triangle(p, color)
+    })
+}
+const topRightTriangle = (x, y, color) => {
+    mirror.topRightCorner(x, y).forEach((p) => {
+        triangle(p, color)
+    })
+}
+const bottomRightTriangle = (x, y, color) => {
+    mirror.bottomRightCorner(x, y).forEach((p) => {
+        triangle(p, color)
+    })
+}
+const bottomLeftTriangle = (x, y, color) => {
+    mirror.bottomLeftCorner(x, y).forEach((p) => {
+        triangle(p, color)
+    })
 }
 
 // Create container
@@ -194,12 +91,6 @@ svgContainer.appendChild(mainSVG)
 const windowFrame = document.getElementById('windowFrame')
 windowFrame.appendChild(svgContainer)
 
-// Setup automata
-const g = new AutomataGrid(24, 24)
-cellSize = {
-    w: svgFrameSize.w / (1 + g.cols * 2),
-    h: svgFrameSize.h / (1 + g.rows * 2)
-}
 const paramBox = document.createElement('div')
 paramBox.id = 'interactiveParameter'
 
@@ -221,18 +112,26 @@ colorButton.addEventListener(
     'click',
     (event) => {
         sketch.changeColor()
-        print()
+        sketch.fillSvg()
     },
     false
 )
 windowFrame.appendChild(paramBox)
+const g = new AutomataGrid(24, 24)
+const mirror = new MirrorShape(g.cols, g.rows)
 
 const sketch = {
+    palette: false,
+    cellSize: {
+        w: svgFrameSize.w / (1 + g.cols * 2),
+        h: svgFrameSize.h / (1 + g.rows * 2)
+    },
+    groupsByColor: [],
     init: () => {
         g.init()
         sketch.changeColor()
         sketch.update()
-        print()
+        sketch.fillSvg()
     },
     update: () => {
         g.update()
@@ -242,32 +141,116 @@ const sketch = {
         if (!aliveCellInGrid) {
             g.init()
         }
-        print()
+        sketch.fillSvg()
+    },
+    emptySvg: () => {
+        for (let i = 0; i < sketch.groupsByColor.length; i++) {
+            while (sketch.groupsByColor[i].firstChild) {
+                sketch.groupsByColor[i].removeChild(
+                    sketch.groupsByColor[i].firstChild
+                )
+            }
+        }
+    },
+    fillSvg: () => {
+        sketch.emptySvg()
+        for (let x = 0; x <= g.cols; x++) {
+            for (let y = 0; y <= g.rows; y++) {
+                const i = x * g.cols + y
+                const colorNum = i % sketch.palette.length
+
+                // top and bootom
+                if (
+                    y > 0 &&
+                    y < g.rows &&
+                    g.value[i - g.rows] &&
+                    g.value[i + g.rows]
+                ) {
+                    topLeftTriangle(x, y, colorNum)
+                    //fillCell(x, y, colorNum)
+                    //fillCell(x, y - 1, colorNum)
+                    //fillCell(x, y + 1, colorNum)
+                }
+                // left & right
+                if (
+                    x > 0 &&
+                    x < g.cols &&
+                    g.value[i + g.cols] &&
+                    g.value[i - g.cols]
+                ) {
+                    bottomRightTriangle(x, y, colorNum)
+                    //fillCell(x, y, colorNum)
+                    //fillCell(x - 1, y, colorNum)
+                    //fillCell(x + 1, y, colorNum)
+                }
+                // top & left
+                if (x > 0 && y > 0 && g.value[i - 1] && g.value[i - g.cols]) {
+                    //fillCell(x, y, colorNum)
+                    topLeftTriangle(x - 1, y - 1, colorNum)
+                }
+                // top & right
+                if (
+                    x < g.cols &&
+                    y > 0 &&
+                    g.value[i - 1] &&
+                    g.value[i + g.cols]
+                ) {
+                    //fillCell(x, y, colorNum)
+                    topRightTriangle(x + 1, y - 1, colorNum)
+                }
+                // bottom & right
+                if (
+                    x < g.cols &&
+                    y < g.rows &&
+                    g.value[i + 1] &&
+                    g.value[i + g.cols]
+                ) {
+                    //fillCell(x, y, i % sketch.palette.length)
+                    bottomRightTriangle(x + 1, y + 1, colorNum)
+                }
+                // bottom & left
+                if (
+                    x > 0 &&
+                    y < g.rows &&
+                    g.value[i + 1] &&
+                    g.value[i - g.cols]
+                ) {
+                    //fillCell(x, y, i % sketch.palette.length)
+                    bottomLeftTriangle(x - 1, y + 1, colorNum)
+                }
+            }
+        }
     },
     changeColor: () => {
-        palette = getRandomPalette(4)
-        //palette = getColorCombination(3, false).colors
-        colorsGroups = []
-        // create group for every color in palette
-        palette.forEach((color) => {
-            colorsGroups.push({
-                colorValue: color.value,
-                colorName: color.name.replace(' ', '_').toLowerCase(),
-                shapes: []
-            })
+        sketch.groupsByColor = []
+        while (mainSVG.firstChild) {
+            mainSVG.removeChild(mainSVG.firstChild)
+        }
+        sketch.palette = getRandomPalette(4)
+        // sketch.palette = getColorCombination(3, false).colors
+        // create group for every color in sketch.palette
+
+        sketch.palette.forEach((color) => {
+            const g = document.createElementNS(
+                'http://www.w3.org/2000/svg',
+                'g'
+            )
+            g.setAttribute('id', color.name.replace(' ', '_').toLowerCase())
+            g.setAttribute('fill', color.value)
+            sketch.groupsByColor.push(g)
+            mainSVG.appendChild(g)
         })
+        sketch.fillSvg()
     },
     downloadSVG: () => {
         if (window.confirm('Do you want to download the SVG file ?')) {
-            const date = new Date()
-            const filename =
-                'Cellular-Automata.' +
-                date.getHours() +
-                '.' +
-                date.getMinutes() +
-                '.' +
-                date.getSeconds() +
-                '--Nicolas_Lebrun.svg'
+            const date = new Date(),
+                Y = date.getFullYear(),
+                m = date.getMonth(),
+                d = date.getDay(),
+                H = date.getHours(),
+                i = date.getMinutes(),
+                filename = `Cells.${Y}-${m}-${d}.${H}-${i}.svg`
 
             exportSVG('frame', filename)
         }
