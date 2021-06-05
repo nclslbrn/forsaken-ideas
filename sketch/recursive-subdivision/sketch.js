bd5105fec6bb73b7ffdb67aee6375a79a6c86d37
nclslbrn <nl@artemg.com>
2021-06-05 15:03:27 +0200

    fix Nan value

--- sketch/recursive-subdivision/sketch.js
+++ sketch/recursive-subdivision/sketch.js
@@ -9,11 +9,11 @@ const randomBetween = (interval = { min: 0, max: 1 }) => {
 
 const sketch = {
     // Main sketch variable
-    thickness: 4,
+    thickness: 2,
     svg: new SvgTracer(document.getElementById('windowFrame'), 'a4'),
     decrease: 0.7,
-    iterations: 36,
-    subSize: { min: 0.3, max: 0.7 },
+    iterations: 256,
+    subSize: { min: 0.2, max: 0.8 },
     nIter: 0,
     divisions: [],
 
@@ -57,53 +57,49 @@ const sketch = {
             const { w, h, x, y, chance, isVert } = props
             if (chance > Math.random()) {
                 if (isVert) {
-                    const nW = Math.round(randomBetween(sketch.subSize) * w)
-                    if (Number.isNaN(nW)) console.log(props)
-                    sketch.subdivision(
-                        nW,
-                        h,
-                        x,
-                        y,
-                        chance * sketch.decrease,
-                        false
-                    )
-                    sketch.subdivision(
-                        w - nW,
-                        h,
-                        x + nW,
-                        y,
-                        chance * sketch.decrease,
-                        false
-                    )
+                    const xDiv = Math.round(randomBetween(sketch.subSize) * w)
+                    sketch.subdivision({
+                        w: xDiv,
+                        h: h,
+                        x: x,
+                        y: y,
+                        chance: chance * sketch.decrease,
+                        isVert: false
+                    })
+                    sketch.subdivision({
+                        w: w - xDiv,
+                        h: h,
+                        x: x + xDiv,
+                        y: y,
+                        chance: chance * sketch.decrease,
+                        isVert: false
+                    })
                 } else {
-                    const nH = Math.round(randomBetween(sketch.subSize) * h)
-                    if (Number.isNaN(nH)) console.log(props)
-                    sketch.subdivision(
-                        w,
-                        nH,
-                        x,
-                        y,
-                        chance * sketch.decrease,
-                        true
-                    )
-                    sketch.subdivision(
-                        w,
-                        h - nH,
-                        x,
-                        y - nH,
-                        chance * sketch.decrease,
-                        true
-                    )
+                    const yDiv = Math.round(randomBetween(sketch.subSize) * h)
+                    sketch.subdivision({
+                        w: w,
+                        h: yDiv,
+                        x: x,
+                        y: y,
+                        chance: chance * sketch.decrease,
+                        isVert: true
+                    })
+                    sketch.subdivision({
+                        w: w,
+                        h: h - yDiv,
+                        x: x,
+                        y: y + yDiv,
+                        chance: chance * sketch.decrease,
+                        isVert: true
+                    })
                 }
             } else {
-                const newDivision = {
+                sketch.divisions.push({
                     x: x + sketch.thickness / 2,
                     y: y + sketch.thickness / 2,
                     w: w - sketch.thickness,
                     h: h - sketch.thickness
-                }
-                console.log('newDivision', newDivision)
-                sketch.divisions.push(newDivision)
+                })
             }
         }
     },
@@ -130,28 +126,14 @@ const sketch = {
             Math.random() * sketch.divisions.length
         )
         let nextDivisionProps = sketch.divisions[randDivisionIndex]
-        if (Number.isNaN(nextDivisionProps.w)) {
-            console.log(
-                '0 > ',
-                randDivisionIndex,
-                ' < ',
-                sketch.divisions.length
-            )
-        }
         nextDivisionProps['chance'] = Math.random()
         nextDivisionProps['isVert'] = Math.random() < 0.5
 
+        sketch.divisions.splice(randDivisionIndex, 1)
         sketch.subdivision({ ...nextDivisionProps })
-        // sketch.divisions.splice(randDivisionIndex)
-        // sketch.print()
+        sketch.print()
 
         if (sketch.nIter < sketch.iterations) {
-            /* console.log(
-                "Iteration doesn't suck",
-                sketch.nIter,
-                '/',
-                sketch.iterations
-            ) */
             requestAnimationFrame(sketch.update)
             sketch.nIter++
         } else {
