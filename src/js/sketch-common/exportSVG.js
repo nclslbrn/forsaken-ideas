6667f241df15e7d55482810642a77be58c8e3b8f
nclslbrn <nl@artemg.com>
2021-06-17 11:40:36 +0200

    comment + ability to pass node ID or node itself

--- src/js/sketch-common/exportSVG.js
+++ src/js/sketch-common/exportSVG.js
@@ -1,17 +1,39 @@
-const exportSVG = (svgContainerId, filename) => {
+import SvgTracer from './svg-tracer'
 
+/**
+ * Simple function to save inline (html) Svg into .SVG file
+ * @param {string|object} svgContainer the svg parent element id or node element
+ * @param {string} sketchName used to prefix the name of the SVG
+ */
+const exportSVG = (svgContainer, sketchName) => {
     let svgFile = null
+    let parent = null
+    const date = new Date(),
+        Y = date.getFullYear(),
+        m = date.getMonth(),
+        d = date.getDay(),
+        H = date.getHours(),
+        i = date.getMinutes()
 
-    const svgContainer = document.getElementById(svgContainerId)
-    var data = new Blob([svgContainer.innerHTML], {
+    const filename = `${sketchName}.${Y}-${m}-${d}_${H}.${i}.svg`
+    if (
+        svgContainer instanceof Element ||
+        svgContainer instanceof HTMLDocument
+    ) {
+        parent = svgContainer
+    } else {
+        parent = document.getElementById(svgContainer)
+    }
+
+    var data = new Blob([parent.innerHTML], {
         type: 'text/plain'
-    });
+    })
     if (svgFile !== null) {
         window.URL.revokeObjectURL(svgFile)
     }
-    svgFile = window.URL.createObjectURL(data);
+    svgFile = window.URL.createObjectURL(data)
 
-    const link = document.createElement("a")
+    const link = document.createElement('a')
     link.href = svgFile
     link.download = filename
     link.click()
