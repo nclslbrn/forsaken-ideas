c4d8a5175f6b45e6769d0c9c5f271fad37de984d
nclslbrn <nl@artemg.com>
2021-07-07 22:45:49 +0200

    make point bounce against the frame

--- sketch/clifford-attractor/clifford-attractor.js
+++ sketch/clifford-attractor/clifford-attractor.js
@@ -1,80 +1,78 @@
 import strangeAttractors from '../../src/js/sketch-common/strange-attractors'
+import Notification from '../../src/js/sketch-common/Notification'
 
 const sketch = (p5) => {
-    const res = 8
+    const res = 2
+    const maxBounce = 2
     const initPoints = []
+    const scale = 0.001
+
     let points = []
     let pointsHistory = []
-    let newLineCrossRef = []
     let canvas
+
     const strokeColor = p5.color(0, 25)
     const width = 1200 //window.innerWidth * 0.75
     const height = 630 //window.innerHeight * 0.75
-    const scale = 0.01
-    const margin = window.innerWidth / 56
     const cliffordAttractor = strangeAttractors(p5).attractors['clifford']
 
     p5.setup = () => {
         canvas = p5.createCanvas(width, height)
         p5.stroke(strokeColor)
 
-        for (let x = margin; x < width - margin; x += res) {
+        for (let x = 0; x < width; x += res) {
             initPoints.push({
                 x: x,
-                y: margin,
+                y: 0,
                 vx: 0,
-                vy: 5
+                vy: 1,
+                bounce: 0
             })
             pointsHistory.push([])
         }
         sketch.init()
     }
     p5.draw = () => {
-        for (let p = 0; p < points.length; p++) {
-            const x = (points[p].x - width / 2) * scale
-            const y = (points[p].y - height / 2) * scale
-            const v = cliffordAttractor(p5.createVector(x, y))
-            const angle = p5.atan2(v.x - x, v.y - y) * 0.5
-            points[p].vx += p5.cos(angle) * 0.01
-            points[p].vy += p5.sin(angle) * 0.01
+        if (points.length !== 0) {
+            for (let p = 0; p < points.length; p++) {
+                const x = points[p].x - width / 2
+                const y = points[p].y - height / 2
+                const v = cliffordAttractor(
+                    p5.createVector(x * scale, y * scale)
+                )
+                const angle = Math.atan2(v.x, v.y) * 1.5
+                points[p].vx += Math.cos(angle)
+                points[p].vy += Math.sin(angle)
+                p5.line(
+                    points[p].x,
+                    points[p].y,
+                    points[p].x + points[p].vx,
+                    points[p].y + points[p].vy
+                )
 
-            p5.line(
-                points[p].x,
-                points[p].y,
-                points[p].x + points[p].vx,
-                points[p].y + points[p].vy
-            )
-
-            if (
-                points[p].x + points[p].vx > width - margin ||
-                points[p].y + points[p].vy > height - margin ||
-                points[p].x + points[p].vx < margin ||
-                points[p].y + points[p].vy < margin
-            ) {
-                newLineCrossRef[p] = pointsHistory.length
-                pointsHistory[pointsHistory.length] = []
-            } else {
-                if (typeof newLineCrossRef[p] != 'undefined') {
-                    pointsHistory[newLineCrossRef[p]].push({
-                        x: points[p].x,
-                        y: points[p].y
-                    })
-                } else {
-                    pointsHistory[p].push({
-                        x: points[p].x,
-                        y: points[p].y
-                    })
+                if (points[p].x > width || points[p].x < 0) {
+                    points[p].vx *= -1
+                    points[p].bounce += 1
                 }
+                if (points[p].y > height || points[p].y < 0) {
+                    points[p].vy *= -1
+                    points[p].bounce += 1
+                }
+                pointsHistory[p].push({
+                    x: points[p].x,
+                    y: points[p].y
+                })
+                points[p].x += points[p].vx * 0.9
+                points[p].y += points[p].vy * 0.9
             }
-            points[p].x += points[p].vx
-            points[p].y += points[p].vy
-            points[p].vx *= 0.99
-            points[p].vy *= 0.99
-
-            if (points[p].x > width - margin) points[p].x = margin
-            if (points[p].y > height - margin) points[p].y = margin
-            if (points[p].x < margin) points[p].x = width - margin
-            if (points[p].y < margin) points[p].y = height - margin
+            points = points.filter((point) => point.bounce < maxBounce)
+        } else {
+            new Notification(
+                'Sketch done !',
+                document.getElementById('windowFrame'),
+                'light'
+            )
+            p5.noLoop()
         }
     }
     sketch.init = () => {
@@ -85,6 +83,8 @@ const sketch = (p5) => {
         pointsHistory = initPoints.map((point) => {
             return []
         })
+        console.log(points.length)
+
         p5.background(255, 250, 245)
     }
     sketch.downloadJPG = () => {
