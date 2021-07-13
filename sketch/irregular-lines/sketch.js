a754f86c0afeab247051eec51b8b091c9ea78903
nclslbrn <nl@artemg.com>
2021-07-13 17:21:24 +0200

    make shape random

--- sketch/irregular-lines/sketch.js
+++ sketch/irregular-lines/sketch.js
@@ -2,8 +2,7 @@ import SvgTracer from '../../src/js/sketch-common/svg-tracer'
 import SimplexNoise from 'simplex-noise'
 import ease from '../../src/js/sketch-common/ease'
 import isPointInsidePolygon from './isPointInsidePolygon'
-// print in 24 * 32 cm
-// from inkscape it correspond to 1209,44885 * 907,08661
+const sketchSize = Math.min(window.innerWidth, window.innerHeight) * 0.75
 const sketch = {
     step: 5,
     margin: 50,
@@ -12,16 +11,18 @@ const sketch = {
     noiseAmplitude: 40,
     lines: [],
     svg: new SvgTracer(document.getElementById('windowFrame'), {
-        w: 1209.44885,
-        h: 907.08661
+        w: sketchSize,
+        h: sketchSize
     }),
+    sides: [2 / 3, 2 / 4, 2 / 5, 2 / 6],
     seed: undefined,
     simplex: undefined,
     isDone: undefined,
+    side: undefined,
+    // triangle square pentagone hexagon
     // setup
     launch: () => {
         sketch.svg.init()
-
         sketch.init()
     },
     // reset value and relaunch drawing
@@ -41,15 +42,18 @@ const sketch = {
             })
             sketch.lines.push([])
         }
+        sketch.side =
+            sketch.sides[Math.floor(sketch.sides.length * Math.random())]
         sketch.isDone = false
         sketch.update()
     },
     // compute change
     update: () => {
-        const radius = 320
+        const radius = sketchSize * 0.35
         const center = { x: sketch.svg.width / 2, y: sketch.svg.height / 2 }
         const polygon = []
-        const rot = Math.PI / 2 //* (2 / 3)
+
+        const rot = Math.PI * sketch.side
         for (let theta = 0; theta < Math.PI * 2; theta += rot) {
             const r =
                 radius *
@@ -66,7 +70,6 @@ const sketch = {
         for (let i = 0; i < sketch.points.length; i++) {
             let amplitude = 0
             let dist = sketch.svg.width
-
             let _d = 1.5
 
             if (isPointInsidePolygon(sketch.points[i], polygon)) {
@@ -81,20 +84,23 @@ const sketch = {
                     if (pointToJ < dist) dist = pointToJ
                 }
                 _d = 1 - dist / (radius * 4)
-                amplitude = ease(_d * _d, 50) * sketch.noiseAmplitude
+                amplitude = ease(_d) * sketch.noiseAmplitude
             }
 
             sketch.points[i].x += sketch.step
+            let x = sketch.points[i].x
             let y = sketch.points[i].y
-            y +=
-                sketch.simplex.noise3D(
-                    sketch.points[i].x / (sketch.noiseFrequency * (_d * 2)),
-                    sketch.points[i].y / (sketch.noiseFrequency * (_d * 2)),
-                    Math.sin(_d) / sketch.noiseFrequency
-                ) * amplitude
+            const noise = sketch.simplex.noise3D(
+                sketch.points[i].x / (sketch.noiseFrequency * (_d * 2)),
+                sketch.points[i].y / (sketch.noiseFrequency * (_d * 2)),
+                Math.sin(_d) / sketch.noiseFrequency
+            )
+
+            x += Math.cos(noise) * amplitude
+            y += Math.sin(noise) * amplitude
 
             if (sketch.lines[i]) {
-                sketch.lines[i].push([sketch.points[i].x, y])
+                sketch.lines[i].push([x, y])
             }
             if (sketch.points[i].x >= sketch.svg.width - sketch.margin) {
                 sketch.isDone = true
