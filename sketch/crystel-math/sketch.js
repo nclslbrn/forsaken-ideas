7efa795f6f54afa13addad10df0abbe2f55c8157
nclslbrn <nl@artemg.com>
2021-07-23 22:29:26 +0200

    add simetry

--- sketch/crystel-math/sketch.js
+++ sketch/crystel-math/sketch.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import AutomataGrid from '../../src/js/sketch-common/AutomataGrid'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { SceneUtils } from 'three/examples/jsm/utils/SceneUtils'
+import Notification from '../../src/js/sketch-common/Notification'
 const windowFrame = document.getElementById('windowFrame')
 
 const s = {
@@ -17,6 +18,9 @@ const s = {
         1,
         20000
     ),
+    object: new THREE.Object3D(),
+    build: new THREE.Object3D(),
+    zRot: 0,
     renderer: new THREE.WebGLRenderer({ antialias: true }),
     launch: () => {
         s.controls = new OrbitControls(s.camera, s.renderer.domElement)
@@ -42,7 +46,7 @@ const s = {
         )
         s.renderer.setPixelRatio(window.devicePixelRatio)
         s.renderer.setSize(window.innerWidth, window.innerHeight)
-        s.camera.position.z = Math.max(s.grid.cols, s.grid.rows) / 2
+        s.camera.position.z = Math.max(s.grid.cols, s.grid.rows) / 4
         s.camera.lookAt(0, 0, 0)
         windowFrame.appendChild(s.renderer.domElement)
         window.addEventListener('resize', s.onWindowResize, false)
@@ -55,6 +59,7 @@ const s = {
         s.grid.update()
         s.scene.background = new THREE.Color(0x000000)
         s.scene.fog = new THREE.FogExp2(0xffffff, 0.07)
+        s.build.name = 'chrystal'
         s.drawgrid()
         s.render()
     },
@@ -68,23 +73,36 @@ const s = {
     },
     animate: () => {
         requestAnimationFrame(s.animate)
+
+        s.zRot++
+        s.build.rotation.set(0, s.zRot / 100, 0)
+
+        if (s.zRot % 30 == 0) {
+            s.grid.update()
+            s.drawgrid()
+        }
+
         if (s.render !== undefined) s.render()
-    },
-    cleanScene: () => {
-        const toKeep = ['DirectionalLight', 'AmbientLight']
-        s.scene.children.forEach((child) => {
-            if (!toKeep.includes(child.type)) {
-                s.scene.remove(child)
-            }
-        })
     },
     addMesh: (x, y) => {
         const clone = s.object.clone(true)
-        clone.position.set((1 / s.grid.cols) * x, (1 / s.grid.rows) * y, 0)
+        clone.position.set(
+            -1 / s.grid.cols + (1 / s.grid.cols) * x,
+            -1 / s.grid.rows + (1 / s.grid.rows) * y,
+            0
+        )
         return clone
     },
     drawgrid: () => {
-        s.cleanScene()
+        s.build.clear()
+        if (s.scene.getObjectByName('chrystal')) {
+            s.scene.remove(s.scene.getObjectByName('chrystal'))
+            /*   new Notification(
+                'Previous construction was removed.',
+                windowFrame,
+                'dark'
+            ) */
+        }
         const group = new THREE.Group()
         for (let x = 0; x <= s.grid.cols; x++) {
             for (let y = 0; y <= s.grid.rows; y++) {
@@ -146,18 +164,30 @@ const s = {
                 }
             }
         }
+
         const xMirror = group.clone(true)
         xMirror.scale.set(-1, 1, 1)
 
-        const yGroup = group.clone(true)
-        yGroup.rotateY(Math.PI / 2)
-        const yMirror = yGroup.clone(true)
+        const zCopy = group.clone(true)
+        zCopy.rotation.set(0, Math.PI / 2, 0)
+
+        const zMirror = group.clone(true)
+        zMirror.rotation.set(0, Math.PI * 1.5, 0)
+
+        const yMirror = new THREE.Group()
+        yMirror.add(group.clone(true))
+        yMirror.add(xMirror.clone(true))
+        yMirror.add(zCopy.clone(true))
+        yMirror.add(zMirror.clone(true))
         yMirror.scale.set(1, -1, 1)
 
-        s.scene.add(group)
-        s.scene.add(xMirror)
-        s.scene.add(yGroup)
-        s.scene.add(yMirror)
+        s.build.add(group)
+        s.build.add(xMirror)
+        s.build.add(zCopy)
+        s.build.add(zMirror)
+        s.build.add(yMirror)
+
+        s.scene.add(s.build)
     }
 }
 export default s
