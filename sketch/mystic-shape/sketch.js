3c19052e1698bf5cc4eb917149e80311b0f9dc00
nclslbrn <nl@artemg.com>
2021-09-14 19:10:48 +0200

    add colors

--- sketch/mystic-shape/sketch.js
+++ sketch/mystic-shape/sketch.js
@@ -1,6 +1,6 @@
+import '../../src/sass/frame-canvas.scss'
 import SvgTracer from '../../src/js/sketch-common/svg-tracer'
 import Notification from '../../src/js/sketch-common/Notification'
-import ease from '../../src/js/sketch-common/ease'
 import funcs from '../../src/js/sketch-common/plane-curve'
 import map from '../../src/js/sketch-common/remap'
 import SimplexNoise from 'simplex-noise'
@@ -15,19 +15,27 @@ const randomTrigoFunc = () => {
 const circle = (theta) => {
     return { x: Math.cos(theta), y: Math.sin(theta) }
 }
-
+// remove 0.8mm for spiral
+const cansonA3 = { w: 1584.37795, h: 1122.51969 }
+const groups = ['white', 'tomato', 'steelblue']
 const container = document.getElementById('windowFrame')
-const tracer = new SvgTracer(container, 'a3', 'black')
+const tracer = new SvgTracer(container, cansonA3, 'black')
 const simplex = new SimplexNoise()
 
 const sketch = {
     iterations: 30,
-    numPoints: 5000,
+    numPoints: 2000,
     margin: 0.2,
     scale: 1,
     // setup
     launch: () => {
         tracer.init()
+        groups.forEach((group) =>
+            tracer.group({
+                name: group,
+                stroke: group
+            })
+        )
         sketch.init()
     },
     // reset value and relaunch drawing
@@ -36,8 +44,6 @@ const sketch = {
         sketch.points = []
         sketch.lines = []
         sketch.trigoFunc = randomTrigoFunc()
-
-        console.log(sketch.trigoFunc)
 
         const rot = (Math.PI * 2) / sketch.numPoints
 
@@ -51,7 +57,6 @@ const sketch = {
             }
             sketch.lines[i] = []
         }
-
         sketch.update()
     },
     // compute change
@@ -63,7 +68,7 @@ const sketch = {
                 //  const v1 = circle(angle)
                 const v1 = funcs[sketch.trigoFunc](point)
                 const a1 = Math.atan2(v1.y, v1.x)
-                const a2 = simplex.noise2D(v1.x, v1.y) - 0.5
+                const a2 = simplex.noise2D(v1.x / 100, v1.y / 100)
                 const v2 = circle(a2)
                 const v3 = {
                     x: Math.cos(v1.x + v2.x),
@@ -102,20 +107,30 @@ const sketch = {
     },
     // push them into <svg>
     draw: () => {
-        tracer.clear()
-        for (const line of sketch.lines) {
+        tracer.clearGroups()
+        for (let i = 0; i < sketch.lines.length; i++) {
             tracer.path({
-                points: line,
+                points: sketch.lines[i],
                 fill: 'none',
-                stroke: 'white'
+                group: groups[i % groups.length]
             })
         }
         tracer.text({
             x: tracer.width * sketch.margin,
             y: tracer.height - tracer.height * sketch.margin * 0.5,
-            text: sketch.trigoFunc.toUpperCase().replaceAll('_', ' '),
+            text:
+                'PLANE CURVE / ' +
+                sketch.trigoFunc.toUpperCase().replaceAll('_', ' '),
             fontSize: 14,
-            fill: 'white'
+            group: 'white'
+        })
+
+        tracer.text({
+            x: tracer.width - tracer.width * sketch.margin * 1.5,
+            y: tracer.height - tracer.height * sketch.margin * 0.5,
+            text: 'N.LEBRUN 14/09/2021',
+            fontSize: 14,
+            group: 'white'
         })
     },
     // export inline <svg> as SVG file
