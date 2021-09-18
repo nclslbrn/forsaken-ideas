605a20d3167d06b392de63ee562a0b6e9d6578f5
nclslbrn <nl@artemg.com>
2021-09-18 21:21:17 +0200

    add stabilo colors

--- sketch/mystic-shape/sketch.js
+++ sketch/mystic-shape/sketch.js
@@ -4,6 +4,7 @@ import Notification from '../../src/js/sketch-common/Notification'
 import funcs from '../../src/js/sketch-common/plane-curve'
 import map from '../../src/js/sketch-common/remap'
 import SimplexNoise from 'simplex-noise'
+import { getColorCombination } from '../../src/js/sketch-common/stabilo68-colors'
 
 const randomTrigoFunc = () => {
     const funcsName = []
@@ -12,34 +13,27 @@ const randomTrigoFunc = () => {
     })
     return funcsName[Math.floor(Math.random() * funcsName.length)]
 }
+
 const circle = (theta) => {
     return { x: Math.cos(theta), y: Math.sin(theta) }
 }
-// remove 0.8mm for A3square (spiral notebook)
-const cansonA3square = { w: 29.7, h: 29.7 }
-const groups = ['white', 'tomato', 'steelblue']
+
 const container = document.getElementById('windowFrame')
+const simplex = new SimplexNoise()
 const tracer = new SvgTracer({
     parentElem: container,
-    size: cansonA3square,
-    background: 'black'
+    size: 'A3_landscape'
 })
-const simplex = new SimplexNoise()
 
 const sketch = {
     iterations: 30,
-    numPoints: 2000,
+    numPoints: 4000,
     margin: 0.2,
     scale: 1,
+    speed: 0.009,
     // setup
     launch: () => {
         tracer.init()
-        groups.forEach((group) =>
-            tracer.group({
-                name: group,
-                stroke: group
-            })
-        )
         sketch.init()
     },
     // reset value and relaunch drawing
@@ -48,19 +42,27 @@ const sketch = {
         sketch.points = []
         sketch.lines = []
         sketch.trigoFunc = randomTrigoFunc()
-
+        sketch.palette = getColorCombination(2)
+        
         const rot = (Math.PI * 2) / sketch.numPoints
-
         for (let i = 0; i < sketch.numPoints; i++) {
             const theta = rot * i
             const pos = { x: Math.cos(theta), y: Math.sin(theta) }
             const disp = funcs[sketch.trigoFunc](pos)
             sketch.points[i] = {
-                x: Math.cos(theta * disp.x),
-                y: Math.sin(theta * disp.y)
+                x: Math.cos(pos.x - disp.x),
+                y: Math.sin(pos.y - disp.y)
             }
             sketch.lines[i] = []
         }
+        tracer.clear()
+        sketch.palette.colors.forEach((color)=> 
+            tracer.group({
+                name: color.id,
+                id: color.id,
+                stroke: color.value
+            })
+        )
         sketch.update()
     },
     // compute change
@@ -73,12 +75,12 @@ const sketch = {
                 const v1 = funcs[sketch.trigoFunc](point)
                 const a1 = Math.atan2(v1.y, v1.x)
                 const a2 = simplex.noise2D(v1.x / 50, v1.y / 50) * 200
-                const v2 = circle(a2)
+                const v2 = circle(a1 + a2)
                 const v3 = {
                     x: Math.cos(v1.x + v2.x),
                     y: Math.sin(v1.y + v2.y)
                 }
-                const v4 = funcs['sinusoidal'](v3)
+                const v4 = v3 //funcs['sinusoidal'](v3)
 
                 sketch.lines[p].push([
                     map(
@@ -97,16 +99,24 @@ const sketch = {
                     )
                 ])
 
-                sketch.points[p].x += v4.x * 0.01
-                sketch.points[p].y += v4.y * 0.01
+                sketch.points[p].x += v4.x * sketch.speed
+                sketch.points[p].y += v4.y * sketch.speed
             }
 
             sketch.nIter++
             requestAnimationFrame(sketch.update)
             sketch.draw()
         } else {
+            sketch.draw()
             console.log('Sketch done')
-            new Notification('Sketch done!', container, 'light')
+            const penSpecs = sketch.palette.colors.reduce((specs, color) => {
+                return specs + `<br> - 88/${color.id} ${color.name}`
+            }, '(Stabilo Art markers)')
+            new Notification(
+                `${sketch.palette.name} palette ${penSpecs}`,
+                container,
+                'light'
+            )
         }
     },
     // push them into <svg>
@@ -116,26 +126,24 @@ const sketch = {
             tracer.path({
                 points: sketch.lines[i],
                 fill: 'none',
-                group: groups[i % groups.length],
-                stroke: groups[i % groups.length]
+                group: sketch.palette.colors[i % sketch.palette.colors.length].id,
+                stroke: sketch.palette.colors[i % sketch.palette.colors.length].value
             })
         }
         tracer.text({
             x: tracer.width * sketch.margin,
             y: tracer.height - tracer.height * sketch.margin * 0.5,
-            text:
-                'PLANE CURVE / ' +
-                sketch.trigoFunc.toUpperCase().replaceAll('_', ' '),
+            text: 'PLANE CURVE',
             fontSize: 14,
-            group: 'white'
+            group: sketch.palette.colors[0].id
         })
 
         tracer.text({
             x: tracer.width - tracer.width * sketch.margin * 1.5,
             y: tracer.height - tracer.height * sketch.margin * 0.5,
-            text: 'N.LEBRUN 14/09/2021',
+            text: sketch.trigoFunc.toUpperCase().replaceAll('_', ' '),
             fontSize: 14,
-            group: 'white'
+            group:  sketch.palette.colors[0].id
         })
     },
     // export inline <svg> as SVG file
