9672d161eca89a5fe11e7d47c072c09e35f9132b
Nicolas Lebrun <nl@artemg.com>
2021-12-31 18:15:39 +0100

    update server  with last fuckin webpack api schema

--- tasks/webpack.devServer.js
+++ tasks/webpack.devServer.js
@@ -1,9 +1,12 @@
 const path = require('path')
 
 module.exports = {
-    historyApiFallback: true,
-    contentBase: path.resolve(__dirname, '../public/'),
+    static: {
+        directory: path.resolve(__dirname, '../public/')
+    },
     port: 8080,
-    compress: true,
-    hot: true
+    compress: true
+
+    // historyApiFallback: true,
+    // contentBase: path.resolve(__dirname, '../public/'),
 }
