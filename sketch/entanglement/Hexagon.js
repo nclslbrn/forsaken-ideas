cffb1067fd3d934d8b3fc6978c3390c4b44c5a24
nclslbrn <nl@artemg.com>
2022-02-15 17:19:50 +0100

    add hex side tracing function

--- sketch/entanglement/Hexagon.js
+++ sketch/entanglement/Hexagon.js
@@ -1,3 +1,8 @@
+const PI = Math.PI,
+    cos = Math.cos,
+    sin = Math.sin,
+    random = Math.random
+
 export default class Hexagon {
     constructor(x, y, r) {
         this.x = x
@@ -14,14 +19,10 @@ export default class Hexagon {
 
     getContour() {
         let points = []
-        for (
-            let theta = Math.PI * 0.5;
-            theta <= Math.PI * 2.5;
-            theta += Math.PI / 3
-        ) {
+        for (let theta = PI * 0.5; theta <= PI * 2.5; theta += PI / 3) {
             points.push([
-                this.x + this.r * Math.cos(theta),
-                this.y + this.r * Math.sin(theta)
+                this.x + this.r * cos(theta),
+                this.y + this.r * sin(theta)
             ])
         }
         return points
@@ -34,57 +35,122 @@ export default class Hexagon {
         ]
     }
     getTopSideStrip(lineSpacing) {
-        let p1 = {
-            x: this.x + this.r * Math.cos(Math.PI * 1.166666),
-            y: this.y + this.r * Math.sin(Math.PI * 1.166666)
-        }
-        let p2 = { x: this.x, y: this.y }
         const lines = []
+        let p1, p2, angle
+        if (random() > 0.5) {
+            // top left to bottom right
+            p2 = { x: this.x, y: this.y }
+            p1 = {
+                x: this.x + this.r * cos(PI * 1.166666),
+                y: this.y + this.r * sin(PI * 1.166666)
+            }
+            angle = PI / 3 - PI * 0.5
+        } else {
+            // top right to bottom left
+            p1 = { x: this.x, y: this.y - this.r }
+            p2 = {
+                x: this.x + this.r * cos(PI * 1.166666),
+                y: this.y + this.r * sin(PI * 1.166666)
+            }
+            angle = PI * 0.166666667
+        }
+
         for (let d = 0; d <= this.r / lineSpacing; d++) {
-            const angle = Math.PI / 3 - Math.PI * 0.5
             lines.push([
                 [p1.x, p1.y],
                 [p2.x, p2.y]
             ])
-            p1.x += Math.cos(angle) * lineSpacing
-            p2.x += Math.cos(angle) * lineSpacing
-            p1.y += Math.sin(angle) * lineSpacing
-            p2.y += Math.sin(angle) * lineSpacing
+            p1.x += cos(angle) * lineSpacing
+            p2.x += cos(angle) * lineSpacing
+            p1.y += sin(angle) * lineSpacing
+            p2.y += sin(angle) * lineSpacing
         }
         return lines
     }
 
     getLeftSideStrip(lineSpacing) {
-        const p1 = {
-            x: this.x + this.r * Math.cos(Math.PI * 0.5),
-            y: this.y + this.r * Math.sin(Math.PI * 0.5)
-        }
-        const p2 = {
-            x: p1.x + this.r * Math.cos(Math.PI * -0.833),
-            y: p1.y + this.r * Math.sin(Math.PI * -0.833)
-        }
         const lines = []
-        for (let h = this.r; h >= 0; h -= lineSpacing) {
-            lines.push([
-                [p1.x, p1.y - h],
-                [p2.x, p2.y - h]
-            ])
+        if (random() > 0.5) {
+            // bottom left to top right
+            const p1 = {
+                x: this.x + this.r * cos(PI * 0.5),
+                y: this.y + this.r * sin(PI * 0.5)
+            }
+            const p2 = {
+                x: p1.x + this.r * cos(PI * -0.833),
+                y: p1.y + this.r * sin(PI * -0.833)
+            }
+            for (let h = this.r; h >= 0; h -= lineSpacing) {
+                lines.push([
+                    [p1.x, p1.y - h],
+                    [p2.x, p2.y - h]
+                ])
+            }
+        } else {
+            // top to bottom
+            const angle = PI * 0.16666
+
+            let p1 = {
+                x: this.x + this.r * cos(PI * -0.833),
+                y: this.y + this.r * sin(PI * -0.833)
+            }
+            let p2 = {
+                x: this.x + this.r * cos(PI * 0.8333),
+                y: this.y + this.r * sin(PI * 0.8333)
+            }
+            for (let h = 0; h <= this.r; h += lineSpacing) {
+                lines.push([
+                    [p1.x, p1.y],
+                    [p2.x, p2.y]
+                ])
+
+                p1.x += cos(angle) * lineSpacing
+                p2.x += cos(angle) * lineSpacing
+                p1.y += sin(angle) * lineSpacing
+                p2.y += sin(angle) * lineSpacing
+            }
         }
         return lines
     }
 
     getRightSideStrip(lineSpacing) {
-        const p1 = { x: this.x, y: this.y }
-        const p2 = {
-            x: this.x + this.r * Math.cos(Math.PI * -0.166666),
-            y: this.y + this.r * Math.sin(Math.PI * -0.166666)
-        }
         const lines = []
-        for (let h = 0; h <= this.r; h += lineSpacing) {
-            lines.push([
-                [p1.x, p1.y + h],
-                [p2.x, p2.y + h]
-            ])
+        if (random() > 0.5) {
+            // bottom left to top right
+            const p1 = { x: this.x, y: this.y }
+            const p2 = {
+                x: this.x + this.r * cos(PI * -0.166666),
+                y: this.y + this.r * sin(PI * -0.166666)
+            }
+            for (let h = 0; h <= this.r; h += lineSpacing) {
+                lines.push([
+                    [p1.x, p1.y + h],
+                    [p2.x, p2.y + h]
+                ])
+            }
+        } else {
+            // top to bottom
+            const angle = PI * -0.16666
+
+            let p1 = {
+                x: this.x,
+                y: this.y
+            }
+            let p2 = {
+                x: this.x,
+                y: this.y + this.r
+            }
+            for (let h = 0; h <= this.r; h += lineSpacing) {
+                lines.push([
+                    [p1.x, p1.y],
+                    [p2.x, p2.y]
+                ])
+
+                p1.x += cos(angle) * lineSpacing
+                p2.x += cos(angle) * lineSpacing
+                p1.y += sin(angle) * lineSpacing
+                p2.y += sin(angle) * lineSpacing
+            }
         }
         return lines
     }
