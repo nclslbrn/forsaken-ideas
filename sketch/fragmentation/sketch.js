977119765532c280f1b180b0269832848fc14cc2
Nicolas Lebrun <nl@artemg.com>
2022-02-26 22:03:51 +0100

    rename untilited

--- sketch/fragmentation/sketch.js
+++ sketch/fragmentation/sketch.js
@@ -1,15 +1,55 @@
 import SvgTracer from '../../src/js/sketch-common/svg-tracer'
+import SimplexNoise from 'simplex-noise'
 import { getLineLineCollision } from './trigonometry'
 import isPointInsidePolygon from './isPointInsidePolygon'
+import dashLine from './dashLine'
 import Part from './Part'
 
 let margin, parts, tileSize
 const svg = new SvgTracer({
-    parentElem: document.getElementById('windowFrame'),
-    size: 'A3_portrait'
-})
-const N = 2,
-    I = 10
+        parentElem: document.getElementById('windowFrame'),
+        size: 'P24x32',
+        dpi: 72
+    }),
+    simplex = new SimplexNoise(),
+    N = Math.ceil(Math.random() * 3),
+    I = 16,
+    lineStep = 6,
+    followLine = (line) => {
+        const points = []
+        const p = [...line[0]]
+        const angle = Math.atan2(
+            line[1][1] - line[0][1],
+            line[1][0] - line[0][0]
+        )
+        const size = Math.sqrt(
+            Math.abs(line[1][0] - line[0][0]) ** 2 +
+                Math.abs(line[1][1] - line[0][1]) ** 2
+        )
+        for (let d = 0; d <= size; d += lineStep * 4) {
+            points.push([
+                p[0] + d * Math.cos(angle),
+                p[1] + d * Math.sin(angle)
+            ])
+        }
+        return points
+    },
+    noiseLine = (line) => {
+        const noisedLine = []
+        const freq = 0.003
+        const turbulence = 15
+        const force = 2
+        line.forEach((pt) => {
+            const nValue =
+                turbulence * simplex.noise2D(pt[0] * freq, pt[1] * freq)
+            noisedLine.push([
+                pt[0] + Math.cos(nValue) * force,
+                pt[1] + Math.sin(nValue) * force
+            ])
+        })
+        return noisedLine
+    }
+
 const sketch = {
     // setup
     launch: () => {
@@ -135,9 +175,14 @@ const sketch = {
         }
     },
     drawTiles: () => {
-        const lineStep = 10
         svg.clear()
         parts.forEach((p, i) => {
+            /* svg.path({
+                points: p.points,
+                stroke: 'black',
+                fill: 'none',
+                close: true
+            }) */
             const min = [
                 Math.min(...p.points.map((pt) => pt[0])),
                 Math.min(...p.points.map((pt) => pt[1]))
@@ -146,6 +191,9 @@ const sketch = {
                 Math.max(...p.points.map((pt) => pt[0])),
                 Math.max(...p.points.map((pt) => pt[1]))
             ]
+            const size = Math.sqrt(
+                Math.abs(max[0] - min[0]) ** 2 + Math.abs(max[1] - min[1]) ** 2
+            )
             const lines = []
             if (i % 2) {
                 for (let x = min[0]; x <= max[0]; x += lineStep) {
@@ -162,14 +210,11 @@ const sketch = {
                     ])
                 }
             }
+            const lineStyle = Math.floor(Math.random() * 6)
             lines.forEach((l) => {
                 const angle = Math.atan2(l[1][1] - l[0][1], l[1][0] - l[0][0])
-                const size = Math.sqrt(
-                    Math.abs(l[1][0] - l[0][0]) ** 2 +
-                        Math.abs(l[1][1] - l[0][1]) ** 2
-                )
 
-                let lastPointInPoly = true
+                let lastPointInPoly = false
                 const linePoly = []
                 for (let d = 0; d <= size; d++) {
                     const pt = [
@@ -179,28 +224,82 @@ const sketch = {
                     const isPointInPoly = isPointInsidePolygon(pt, p.points)
                     if (isPointInPoly !== lastPointInPoly) {
                         linePoly.push(pt)
+                        if (linePoly.length == 2) break
                         lastPointInPoly = isPointInPoly
                     }
                 }
-                if (linePoly.length > 0) {
-                    svg.path({
-                        points: linePoly,
-                        stroke: p.index % 2 === 0 ? 'tomato' : 'steelblue',
-                        close: false
-                    })
+                if (linePoly.length == 2) {
+                    switch (lineStyle) {
+                        // dash noise
+                        case 0:
+                            dashLine(
+                                [[...linePoly[0]], [...linePoly[1]]],
+                                32,
+                                p.index % 2
+                            ).forEach((l) =>
+                                svg.path({
+                                    points: [...l].map((pt) => noiseLine(pt)),
+                                    stroke: 'black',
+                                    close: false
+                                })
+                            )
+                            break
+                        // full noise
+                        case 1:
+                            svg.path({
+                                points: noiseLine(linePoly),
+                                stroke: 'black',
+                                close: false
+                            })
+
+                            break
+                        // uniform dash straight
+                        case 2:
+                            dashLine(
+                                [[...linePoly[0]], [...linePoly[1]]],
+                                32,
+                                0
+                            ).forEach((l) =>
+                                svg.path({
+                                    points: [...l],
+                                    stroke: 'black',
+                                    close: false
+                                })
+                            )
+                            break
+                        // random dash straight
+                        case 3:
+                            dashLine(
+                                [[...linePoly[0]], [...linePoly[1]]],
+                                32,
+                                1
+                            ).forEach((l) =>
+                                svg.path({
+                                    points: [...l],
+                                    stroke: 'black',
+                                    close: false
+                                })
+                            )
+                            break
+                        // empty
+                        case 4:
+                            break
+                        // straight line
+                        case 5:
+                            svg.path({
+                                points: linePoly,
+                                stroke: 'black',
+                                close: false
+                            })
+                            break
+                    }
                 }
             })
-            /*  svg.path({
-                points: p.points,
-                fill: p.index % 2 === 0 ? 'black' : 'white',
-                stroke: p.index % 2 === 0 ? 'white' : 'black',
-                close: true
-            }) */
         })
     },
     // export inline <svg> as SVG file
     export: () => {
-        sketch.svg.export({ name: 'sketchname' })
+        svg.export({ name: 'slice' })
     }
 }
 
