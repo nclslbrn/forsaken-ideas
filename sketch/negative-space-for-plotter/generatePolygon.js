bd0d83828b01067b0e1010d7f92a7ce99876fc90
Nicolas Lebrun <nl@artemg.com>
2023-10-07 09:52:02 +0200

    final touch + export for plotting on 24x32cm

--- sketch/negative-space-for-plotter/generatePolygon.js
+++ sketch/negative-space-for-plotter/generatePolygon.js
@@ -5,13 +5,24 @@ import { SYSTEM } from '@thi.ng/random'
 const polyFromTopAndBottom = (top, bottom, ground, colors, yID) => {
     return polygon(
         [
-        ...top.map((p) => [p[0], p[1] - ground / 2]),
-        ...bottom.reverse().map((p) => [p[0], p[1] + ground / 2])
-    ],
-    {
-        fill: colors[yID % colors.length],
-        stroke: '#333'
-    })
+            ...top.map((p) => [p[0], p[1] - ground / 2]),
+            ...bottom.reverse().map((p) => [p[0], p[1] + ground / 2])
+        ],
+        {
+            fill: colors[yID % colors.length]
+            //stroke: '#333'
+        }
+    )
+}
+const lineFromTopAndBottom = (top, bottom, ground) => {
+    if (top[1] < bottom[1]) {
+        top[1] -= ground / 2
+        bottom[1] += ground / 2
+    } else {
+        top[1] += ground / 2
+        bottom[1] -= ground / 2
+    }
+    return line(top, bottom)
 }
 
 const generatePolygon = (
@@ -31,18 +42,18 @@ const generatePolygon = (
     let drawWithLine = false,
         yID = 0
 
-    for (let y = margin; y < height - margin; y += step) {
+    for (let y = margin[1] + step; y < height - margin[1]; y += step) {
         let top = [],
             bottom = []
 
-        for (let x = margin; x < width - margin; x += 3) {
+        for (let x = margin[0]; x < width - margin[0]; x += 3) {
             const n1 = noise2D((x / height) * step * scale, decay)
             const n2 = noise2D((y / height) * step * scale, decay)
             const p1 = [x, y + Math.min(step / 4, n1 * step * 0.5)]
             const p2 = [x, y + Math.min(step / 4, n2 * step * 0.5)]
-            
+
             if (drawWithLine) {
-                lines.push(line(p1, p2))
+                lines.push(lineFromTopAndBottom(p1, p2, ground))
             } else {
                 if (p1[1] < p2[1]) {
                     top.push(p1)
@@ -54,18 +65,20 @@ const generatePolygon = (
             }
             if (SYSTEM.float() > 0.95) {
                 if (!drawWithLine) {
-                    polys.push(polyFromTopAndBottom(top, bottom, ground, colors, yID))
+                    polys.push(
+                        polyFromTopAndBottom(top, bottom, ground, colors, yID)
+                    )
                     top = []
                     bottom = []
                     x += ground
                 }
                 drawWithLine = !drawWithLine
-            }  
+            }
         }
         if (!drawWithLine) {
             polys.push(polyFromTopAndBottom(top, bottom, ground, colors, yID))
         }
-        
+
         yID++
     }
 
--- sketch/negative-space-for-plotter/index.js
+++ sketch/negative-space-for-plotter/index.js
@@ -1,7 +1,7 @@
-import '../full-canvas.css'
+import '../framed-canvas.css'
 import infobox from '../../sketch-common/infobox'
 import handleAction from '../../sketch-common/handle-action'
-import { SYSTEM, pickRandom } from '@thi.ng/random'
+import { SYSTEM } from '@thi.ng/random'
 import { downloadWithMime } from '@thi.ng/dl-asset'
 import { group, rect, asSvg, svgDoc } from '@thi.ng/geom'
 import { FMT_yyyyMMdd_HHmmss } from '@thi.ng/date'
@@ -13,41 +13,46 @@ const dpr = window.devicePixelRatio || 1,
     windowFrame = document.getElementById('windowFrame'),
     loader = document.getElementById('loading'),
     canvas = document.createElement('canvas'),
-    ctx = canvas.getContext('2d'),
-    margin = 50
+    ctx = canvas.getContext('2d')
 
 let decay = 0,
+    margin = [50, 0],
     composition
 
-canvas.width = window.innerWidth * dpr
-canvas.height = window.innerHeight * dpr
+canvas.width = 907.087 //window.innerWidth * dpr
+canvas.height = 1209.449 //window.innerHeight * dpr * ratio
 windowFrame.appendChild(canvas)
 
 const main = () => {
     const palette = tome.get(),
         step = Math.round(SYSTEM.minmax(0.03, 0.1) * canvas.height),
         ground = Math.round(step / SYSTEM.minmax(6, 12)),
-        scale = SYSTEM.minmax(0.5, 0.75),
-        rectPerColRow = Math.round(SYSTEM.minmaxInt(8, 24) / 2) * 2 + 1,
-        factor = pickRandom([3, 5, 6, 7, 9, 10, 11, 12, 13, 15]),
-        [polys, lines] = generatePolygon(
-            step,
-            scale,
-            ground,
-            margin,
-            decay,
-            canvas.width,
-            canvas.height,
-            palette.colors
-        ) 
+        scale = SYSTEM.minmax(0.5, 0.75)
+
+    margin[1] = (canvas.height % (Math.floor(canvas.height / step) * step)) / 2
+
+    const [polys, lines] = generatePolygon(
+        step,
+        scale,
+        ground,
+        margin,
+        decay,
+        canvas.width,
+        canvas.height,
+        palette.colors
+    )
     composition = group({}, [
         rect([canvas.width, canvas.height], { fill: '#f0f3f2' }),
         group({}, polys),
-        group({ stroke: '#333'}, lines)
+        group({ stroke: '#333' }, lines)
     ])
     draw(ctx, composition)
-
-    console.log({ step, factor, rectPerColRow, ground })
+    const logColor = {
+        sign: palette.colors.map(() => '%c  '),
+        style: palette.colors.map((col) => `background: ${col};`)
+    }
+    console.log(logColor.sign.join(' '), ...logColor.style)
+    console.log({ step, ground })
 }
 main()
 windowFrame.removeChild(loader)
