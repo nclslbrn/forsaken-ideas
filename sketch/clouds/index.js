8bb182950890fc547f7d923c788269b817f14937
Nicolas Lebrun <nl@artemg.com>
2024-01-11 23:19:56 +0100

    fix mispelling

--- sketch/clouds/index.js
+++ sketch/clouds/index.js
@@ -20,17 +20,18 @@ const s = params.has('dim')
           .map((v) => parseInt(v))
     : [960, 1200]
 
-const prgssbr = (p5) => {
+const clouds = (p5) => {
     let maxRadius, c, a, palette, canvas, step, noise
 
     p5.setup = () => {
-        canvas = p5.createCanvas(...s)
-        canvas.elt.style.aspectRatio = `${s[0]} / ${s[1]}`
-        maxRadius = min(...s) * 0.5
-        c = s.map((d) => d / 2)
-        prgssbr.init()
+      canvas = p5.createCanvas(...s)
+      canvas.elt.style.aspectRatio = `${s[0]} / ${s[1]}`
+      maxRadius = min(...s) * 0.5
+      c = s.map((d) => d / 2)
+      clouds.init()
     }
-    prgssbr.init = () => {
+    
+    clouds.init = () => {
         noise = createNoise2D()
         palette = getPalette()
         a = round(p5.random(3, 5))
@@ -51,7 +52,7 @@ const prgssbr = (p5) => {
         for (let i = 0; i < a; i++) {
             const r = step * (i + 1)
             const iba = 0.5 + (0.5 * (i + 1)) / a
-            //p5.stroke(palette.stroke)
+
             for (let l = 0; l < PI * 2; l += res) {
                 const n = 0.25 + 0.75 * noise(cos(l) * iba, sin(l) * iba)
                 let p = [c[0] + r * n * Math.cos(l), c[1] + r * n * Math.sin(l)]
@@ -76,15 +77,15 @@ const prgssbr = (p5) => {
         )
     }
 
-    prgssbr.capture = () => {
+    clouds.capture = () => {
         const d = new Date()
-        p5.save(canvas, `Progress-bar.${d.toISOString()}`, 'jpg')
+        p5.save(canvas, `Clouds.${d.toISOString()}`, 'jpg')
     }
 }
 
-new p5(prgssbr, containerElement)
+new p5(clouds, containerElement)
 containerElement.removeChild(loader)
 window.infobox = infobox
-window.init = prgssbr.init
-window.capture = prgssbr.capture
+window.init = clouds.init
+window.capture = clouds.capture
 handleAction()
