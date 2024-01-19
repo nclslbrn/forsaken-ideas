9014fbc08c56170a5e3cad71072ca75733d03615
Nicolas Lebrun <nl@artemg.com>
2024-01-19 19:13:45 +0100

    remove cdn debug message

--- tasks/writeSetup.mjs
+++ tasks/writeSetup.mjs
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import readJson from './read-json.mjs'
-import getCdnTags from './cdn-script-tags.mjs';
-import titleFromSlug from './title-from-slug.mjs';
+import getCdnTags from './cdn-script-tags.mjs'
+import titleFromSlug from './title-from-slug.mjs'
 import stripTag from './strip-tag.mjs'
 import getMenuItem from './additional-menu-items.mjs'
 
@@ -9,52 +9,62 @@ const DEBUGG = true
 
 // Write vite entry point (index.html)
 const writeIndexHTML = async (src, sketchDir) => {
-  const title = titleFromSlug(src)
-  const sketchProps = await readJson(`${sketchDir}/property.json`);
-  const siteProps = await readJson('site-meta.json');
-  sketchProps.description = stripTag(sketchProps.info)
+    const title = titleFromSlug(src)
+    const sketchProps = await readJson(`${sketchDir}/property.json`)
+    const siteProps = await readJson('site-meta.json')
+    sketchProps.description = stripTag(sketchProps.info)
 
-  console.log(sketchProps.libs)
-  const cdnScriptTags = getCdnTags(sketchProps.libs)
-  console.log(cdnScriptTags)
-  const additionalMenuItems = getMenuItem(sketchProps.action)
-  const toInject = { src, title, ...sketchProps, ...siteProps, cdnScriptTags, additionalMenuItems }
+    const cdnScriptTags = getCdnTags(sketchProps.libs)
+    const additionalMenuItems = getMenuItem(sketchProps.action)
+    const toInject = {
+        src,
+        title,
+        ...sketchProps,
+        ...siteProps,
+        cdnScriptTags,
+        additionalMenuItems
+    }
 
-  fs.readFile('./sketch-template.html', 'utf-8', function (err, contents) {
-    if (DEBUGG && err) console.error(err)
-    let replaced = contents
-    Object.keys(toInject).map(key => {
-      const regex = new RegExp(`{{${key}}}`, 'g')
-      replaced = replaced.replaceAll(regex, toInject[key])
+    fs.readFile('./sketch-template.html', 'utf-8', function (err, contents) {
+        if (DEBUGG && err) console.error(err)
+        let replaced = contents
+        Object.keys(toInject).map((key) => {
+            const regex = new RegExp(`{{${key}}}`, 'g')
+            replaced = replaced.replaceAll(regex, toInject[key])
+        })
+        fs.writeFile(
+            `${sketchDir}/index.html`,
+            replaced,
+            { encoding: 'utf-8', flag: 'w' },
+            function (err) {
+                if (DEBUGG && err) console.error(err)
+                return true
+            }
+        )
     })
-    fs.writeFile(
-      `${sketchDir}/index.html`, 
-      replaced, 
-      { encoding: 'utf-8', flag:'w' }, 
-      function (err) {
-      if (DEBUGG && err) console.error(err)
-      return true
-    })
-  })
 }
 
 // Write vite config
 const writeViteConfigJs = (src, sketchDir) => {
-  fs.readFile('./tasks/sketch.vite.config.js', 'utf-8', function (err, contents) {
-    if (DEBUGG && err) console.error(err)
-    const regex = new RegExp('{{src}}', 'g')
-    const replaced = contents.replaceAll(regex, src)
-    fs.writeFile(
-      `${sketchDir}/vite.config.js`, 
-      replaced, 
-      {encoding: 'utf-8', flag:'w'}, 
-      function (err) {
-      if (DEBUGG && err) console.error(err)
-      // console.log(`${sketchDir}/vite-config.js saved`)
-      return true
-    })
-  })
+    fs.readFile(
+        './tasks/sketch.vite.config.js',
+        'utf-8',
+        function (err, contents) {
+            if (DEBUGG && err) console.error(err)
+            const regex = new RegExp('{{src}}', 'g')
+            const replaced = contents.replaceAll(regex, src)
+            fs.writeFile(
+                `${sketchDir}/vite.config.js`,
+                replaced,
+                { encoding: 'utf-8', flag: 'w' },
+                function (err) {
+                    if (DEBUGG && err) console.error(err)
+                    // console.log(`${sketchDir}/vite-config.js saved`)
+                    return true
+                }
+            )
+        }
+    )
 }
-
 
 export { writeIndexHTML, writeViteConfigJs }
