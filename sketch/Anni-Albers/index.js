d828dd8d7594c8eb443717b07816161b80dc7d95
nclslbrn <nclslbrn@gmail.com>
2024-01-19 15:06:43 +0100

    Anni Albers

--- sketch/Anni-Albers/index.js
+++ sketch/Anni-Albers/index.js
@@ -1,58 +1,81 @@
 import p5 from 'p5'
-import { getPalette, albers, alys, martin, mura, freud } from '@nclslbrn/artistry-swatch'
+import {
+    klint,
+    newman,
+    albers,
+    alys,
+    martin,
+    mura,
+    freud
+} from '@nclslbrn/artistry-swatch'
 import '../framed-canvas.css'
 import infobox from '../../sketch-common/infobox'
 import handleAction from '../../sketch-common/handle-action'
-import { RGB_S3TC_DXT1_Format } from 'three'
 
 const windowFrame = document.getElementById('windowFrame')
 const loader = document.getElementById('loading')
-let canvas, palette, margin
+let canvas, palette, margin, base
+
+const { min, random, round } = Math
 
 const sketch = (p5) => {
-    const _x = (x) => x * (p5.width - margin * 2),
-        _y = (y) => y * (p5.height - margin * 2),
-        stack = (num) => {
-            let o = []
-            for (let i = 0; i < num; i++) {
-                o.push(Math.random())
+    const stack = (len, max) => {
+            let o = [],
+                sum = 0
+            while (sum <= len) {
+                const r = random() * max
+                const v = round(r / base) * base
+                o.push(min(len - sum, v))
+                sum += v
             }
-            const sum = o.reduce((a, v) => a + v)
-            return o.map((v) => v / sum)
+            return o
         },
         drawCell = (x, y, w, h, b) => {
-            const cols = w / b,
+            const oneOfTwo = Math.random() > 0.75,
+                cols = w / (b * 2),
                 rows = h / b
+
             for (let c = 0; c < cols; c++) {
                 for (let r = 0; r < rows; r++) {
-                    //if (r % 2 === 0 && c % 2 ==! 0) {
-                        const idx = c * Math.floor(cols) + r
-                        p5.fill(palette.colors[idx % palette.colors.length])
-                        p5.rect(x + c * b, y + r * b, b-2, b-2)
-                    //}
+                    const idx = c * Math.floor(cols) + r
+                    if (!oneOfTwo && idx % 2 === 0) {
+                        const color =
+                                palette.colors[
+                                    r % min(2, palette.colors.length)
+                                ],
+                            dx = c * b * 2,
+                            dy = r * b
+                        p5.fill(color)
+                        p5.rect(
+                            x + dx,
+                            y + dy,
+                            dx + b * 2 > w ? w - dx : b * 2,
+                            dy + b > h ? h - dy : b
+                        )
+                    }
                 }
             }
         }
 
     sketch.init = function () {
-        palette = getPalette()
+        base = p5.random([4, 8, 16]) * 4
+        palette = p5.random([klint, newman, albers, alys, martin, mura, freud])
         //p5.random([albers, alys, martin, mura, freud])
         p5.background(palette.background)
-        p5.stroke(palette.background)
+        p5.noStroke()
+        //p5.stroke(palette.background)
 
-        const yRange = stack(p5.random(5, 10))
+        const yRange = stack(p5.height - margin * 2, 256)
         let y = margin
 
         for (let i = 0; i < yRange.length; i++) {
-            const dy = _y(yRange[i])
-            const xRange = stack(p5.random(5, 10))
+            const dy = yRange[i]
+            const xRange = stack(p5.width - margin * 2, 256)
             let x = margin
             for (let j = 0; j < xRange.length; j++) {
-                const dx = _x(xRange[j])
-                p5.fill(palette.colors[(i + j) % palette.colors.length])
-                //p5.rect(x, y, dx, dy)
-
-                drawCell(x, y, dx, dy, margin * 0.2)
+                const dx = xRange[j]
+                palette.colors = p5.shuffle(palette.colors)
+                drawCell(x, y, dx, dy, base / 2)
                 x += dx
             }
             y += dy
@@ -72,7 +95,7 @@ const sketch = (p5) => {
         p5.save(canvas, filename, 'jpg')
     }
     p5.setup = function () {
-        canvas = p5.createCanvas(1200, 1600)
+        canvas = p5.createCanvas(1200, 1200)
         margin = Math.min(p5.width, p5.height) * 0.07
         sketch.init()
     }
@@ -84,3 +107,4 @@ window.init = sketch.init
 window.jpg = sketch.jpg
 window.infobox = infobox
 handleAction()
+
