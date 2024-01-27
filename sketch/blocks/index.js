65a2834d0ab62c86fdd828f6a716d5f2311cafa8
Nicolas Lebrun <nl@artemg.com>
2024-01-27 22:57:00 +0100

    blocks

--- sketch/blocks/index.js
+++ sketch/blocks/index.js
@@ -1,6 +1,6 @@
 import p5 from 'p5'
-import { getPalette } from '@nclslbrn/artistry-swatch'
-import './physicsjs-full'
+//import { getPalette } from '@nclslbrn/artistry-swatch'
+import PALETTES from './palette'
 import '../framed-canvas.css'
 import infobox from '../../sketch-common/infobox'
 import handleAction from '../../sketch-common/handle-action'
@@ -8,7 +8,7 @@ import handleAction from '../../sketch-common/handle-action'
 const windowFrame = document.getElementById('windowFrame')
 const loader = document.getElementById('loading')
 let canvas, palette, margin
-console.log(window.Physics)
+
 const sketch = (p5) => {
     const _x = (x) => x * (p5.width - margin * 2),
         _y = (y) => y * (p5.height - margin * 2),
@@ -25,20 +25,22 @@ const sketch = (p5) => {
                 rows = h / b
             for (let c = 0; c < cols; c++) {
                 for (let r = 0; r < rows; r++) {
-                    //if (r % 2 === 0 && c % 2 ==! 0) {
-                        const idx = c * Math.floor(cols) + r
-                        p5.fill(palette.colors[idx % palette.colors.length])
-                        p5.rect(x + c * b, y + r * b, b-2, b-2)
-                    //}
+                    const idx = c * Math.floor(cols) + r
+                    p5.fill(palette[idx % palette.length])
+                    p5.rect(x + c * b, y + r * b, b-2, b-2)
                 }
             }
         }
 
     sketch.init = function () {
-        palette = getPalette()
-        //p5.random([albers, alys, martin, mura, freud])
-        p5.background(palette.background)
-        p5.stroke(palette.background)
+        palette = [...(p5.random(PALETTES)).split(';:')]
+        p5.drawingContext.save()
+        p5.background(palette[0])
+        p5.stroke(palette[0])
+        p5.fill(palette[0])
+        palette.splice(0, 1)
+        p5.rect(margin, margin, p5.width - margin * 2, p5.height - margin * 2)
+        p5.drawingContext.clip()
 
         const yRange = stack(p5.random(5, 10))
         let y = margin
@@ -49,20 +51,18 @@ const sketch = (p5) => {
             let x = margin
             for (let j = 0; j < xRange.length; j++) {
                 const dx = _x(xRange[j])
-                p5.fill(palette.colors[(i + j) % palette.colors.length])
-                //p5.rect(x, y, dx, dy)
-
-                drawCell(x, y, dx, dy, margin * 0.2)
+                drawCell(x, y, dx, dy, margin * 0.15)
                 x += dx
             }
             y += dy
         }
+        p5.drawingContext.restore()
     }
 
     sketch.jpg = function () {
         const date = new Date()
         const filename =
-            'Physics.' +
+            'Blocks.' +
             '-' +
             date.getHours() +
             '.' +
