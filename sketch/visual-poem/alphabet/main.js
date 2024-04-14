b450538f4d85bc884bb0420ecfea207e92fc09df
nclslbrn <nclslbrn@gmail.com>
2024-04-14 10:53:26 +0200

    fix lowercase height + try to smooth glyph with auto curve

--- sketch/visual-poem/alphabet/main.js
+++ sketch/visual-poem/alphabet/main.js
@@ -5,7 +5,7 @@
  * Modified version of sam.6"s one
  * @url https://gist.github.com/sam.6/909e0f73d66a0d32b06b17ea77c2959b
  */
-
+import { polyline, asCubic, pathFromCubics } from '@thi.ng/geom'; 
 import { lowercase } from './lowercase';
 import { uppercase } from './uppercase';
 import { ponctuation } from './ponctuation';
@@ -15,9 +15,20 @@ const alphabet = { ...lowercase, ...uppercase, ...ponctuation, ...number }
 
 const getGlyph = (key) => {
   if (alphabet.hasOwnProperty(key)) {
-    return alphabet[key];
+    return softenGlyph(alphabet[key], key);
   } else {
     return [];
+  }
+}
+
+const softenGlyph = (glyph, key) => {
+  const cubics = glyph.map((line) => asCubic(polyline(line)))
+
+  console.log(key, cubics)
+  if (cubics.reduce((con, val) => con && val !== undefined, false)) {
+    return cubics.map(c =>pathFromCubics(c).segments)
+  } else {
+    return glyph
   }
 }
 
