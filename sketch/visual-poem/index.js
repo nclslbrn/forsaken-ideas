0459e8c8ccd6a9b99a18c86ff2aa7da4ab834911
nclslbrn <nclslbrn@gmail.com>
2024-04-16 18:20:04 +0200

    trying with several texts

--- sketch/visual-poem/index.js
+++ sketch/visual-poem/index.js
@@ -1,50 +1,77 @@
-// import { downloadCanvas, downloadWithMime } from '@thi.ng/dl-asset'
-import { rect, line, asSvg, group, polyline, svgDoc } from '@thi.ng/geom'
-// import { repeatedly } from '@thi.ng/transducers'
+import { rect, group, polyline, svgDoc, asSvg } from '@thi.ng/geom'
 import { $compile } from '@thi.ng/rdom'
 import { convertTree } from '@thi.ng/hiccup-svg'
 import { alphabet, getGlyph } from './alphabet/main'
-// import { glyphs } from './glyphs'
 import '../full-canvas.css'
 import infobox from '../../sketch-common/infobox'
 import handleAction from '../../sketch-common/handle-action'
-// import { subdiv } from './subdiv'
-// import { SYSTEM } from '@thi.ng/random'
+import { downloadCanvas, downloadWithMime } from '@thi.ng/dl-asset'
+import { draw } from '@thi.ng/hiccup-canvas'
+import { QUOTE } from './star-wars-quote';
 
 const WIDTH = window.innerWidth,
     HEIGHT = window.innerHeight,
     GRID = [],
-    CELL = [WIDTH / 15, HEIGHT / 9],
-    ROOT = document.getElementById('windowFrame')
-
-Array.from(Object.keys(alphabet)).forEach((char, i) => {
-    if (i % 13 === 0) {
-        GRID.push([char])
-    } else {
-        GRID[Math.floor(i / 13)].push(char)
+    CELL = [
+      10 * Math.ceil(Math.random() * 10), 
+      10 * Math.ceil(Math.random() * 10)
+    ],
+    ROOT = document.getElementById('windowFrame'),
+    RAND_GRID = Math.random() > 0.15,
+    TABLE_SIZE = [
+      Math.floor(WIDTH / CELL[0]) - 2,  
+      Math.floor(HEIGHT / CELL[1]) - 2
+    ]
+/**
+ * Build a 2D array of chars (from alphabet key)
+ */
+const glyphList = Array.from(Object.keys(alphabet))
+if (!RAND_GRID) {
+  glyphList.forEach((char, i) => {
+      if (i % TABLE_SIZE[0] === 0) {
+          GRID.push([char])
+      } else {
+          GRID[Math.floor(i / TABLE_SIZE[0])].push(char)
+      }
+  })
+} else {
+  const randChars = QUOTE[Math.floor(Math.random() * QUOTE.length)]
+  for (let i = 0; i < TABLE_SIZE[1]; i++) {
+    const row = []
+    for (let j = 0; j < TABLE_SIZE[0]; j++) {
+      row.push(randChars[(i * (TABLE_SIZE[0]) + j) % randChars.length])
     }
-})
+    GRID.push(row)
+  }
+}
 
 const signs = []
 GRID.forEach((row, y) =>
     row.forEach((glyph, x) => {
-        const lines = getGlyph(glyph);
-        return lines.map((path) => {
+        const lines = getGlyph(glyph)
+        lines.map((segment) => {
             signs.push(
                 ...[
                     polyline(
-                        path.map((p) => [
+                        segment.map((p) => [
                             p[0] * CELL[0] + (x + 1) * CELL[0],
                             p[1] * CELL[1] + (y + 1) * CELL[1]
                         ])
                     ),
                     // |_
-                    polyline([
-                        [(x + 1) * CELL[0], (y + 1) * CELL[1]],
-                        [(x + 1) * CELL[0], (y + 2) * CELL[1]],
-                        [(x + 2) * CELL[0], (y + 2) * CELL[1]],
-                    ], { stroke: '#333' })
-                    
+                    polyline(
+                        (x > 0 
+                        ? [
+                            [(x + 1) * CELL[0], (y + 1) * CELL[1]],
+                            [(x + 1) * CELL[0], (y + 2) * CELL[1]],
+                            [(x + 2) * CELL[0], (y + 2) * CELL[1]]
+                          ]
+                        : [
+                            [(x + 1) * CELL[0], (y + 2) * CELL[1]],
+                            [(x + 2) * CELL[0], (y + 2) * CELL[1]]
+                        ]),
+                        { stroke: '#333' }
+                    )
                 ]
             )
         })
@@ -58,6 +85,7 @@ const text = [
   : 'Lettres écrites en blanc sur fond noir placées au sein de chaque case d\'une grille modulaire dans l\'ordre de cette énoncé')
 ]
 */
+
 
 const init = () => {
     $compile(
@@ -87,7 +115,58 @@ const init = () => {
 }
 init()
 window.init = init
-// window.capture = sketch.exportJPG
+
+window.capture = () => {
+    const cnvs = document.createElement('canvas')
+    cnvs.width = WIDTH
+    cnvs.height = HEIGHT
+    const ctx = cnvs.getContext('2d')
+    draw(
+        ctx,
+        group({}, [
+            rect([WIDTH, HEIGHT], { fill: '#111111' }),
+            group(
+                {
+                    lineJoin: 'round',
+                    lineCap: 'round',
+                    stroke: '#fefefe',
+                    fill: 'rgba(0,0,0,0)',
+                    weight: 2
+                },
+                signs
+            )
+        ])
+    )
+    downloadCanvas(cnvs, 'visual-poem', 'jpeg')
+}
+
+window.downloadSVG = () => {
+    downloadWithMime(
+        'visual-poem.svg',
+        asSvg(
+            svgDoc(
+                {
+                    width: WIDTH,
+                    height: HEIGHT,
+                    viewBox: `0 0 ${WIDTH} ${HEIGHT}`
+                },
+                group({}, [
+                    rect([WIDTH, HEIGHT], { fill: '#111' }),
+                    group(
+                        {
+                            lineJoin: 'round',
+                            lineCap: 'round',
+                            stroke: '#fefefe',
+                            fill: 'rgba(0,0,0,0)',
+                            weight: 2
+                        },
+                        signs
+                    )
+                ])
+            )
+        )
+    )
+}
 window.infobox = infobox
 handleAction()
 
---
+++ sketch/visual-poem/star-wars-quote.js
@@ -0,0 +1,10 @@
+const QUOTE = [
+  'Rebellions are built on hope.',
+  'The ability to speak does not make you intelligent.',
+  'Try not. Do or do not. There is no try.',
+  'Your eyes can deceive you; don\'t trust them.',
+  'Your focus determines your reality.',
+  'We\'ll always be with you. No one’s ever really gone. A thousand generations live in you now.',
+]
+
+export { QUOTE }
