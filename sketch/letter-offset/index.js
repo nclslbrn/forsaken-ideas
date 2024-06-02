c527132445956064e7861d9799e4a2d5ef1e6296
Nicolas Lebrun <nl@artemg.com>
2024-06-02 20:42:43 +0200

    clean + 2 projects/examples of plot-writer

---
+++ sketch/letter-offset/index.js
@@ -0,0 +1,151 @@
+import '../framed-canvas.css'
+import infobox from '../../sketch-common/infobox'
+import handleAction from '../../sketch-common/handle-action'
+import { getGlyphVector } from '@nclslbrn/plot-writer'
+import { repeatedly, range } from '@thi.ng/transducers'
+import { polyline, rect, group, svgDoc, asSvg } from '@thi.ng/geom'
+import { FMT_yyyyMMdd_HHmmss } from '@thi.ng/date'
+import { downloadCanvas, downloadWithMime } from '@thi.ng/dl-asset'
+import { asPolygons, asSDF, sample2d } from '@thi.ng/geom-sdf'
+import { draw } from '@thi.ng/hiccup-canvas'
+import { SYSTEM, pickRandom } from '@thi.ng/random'
+import { $compile } from '@thi.ng/rdom'
+import { canvas } from '@thi.ng/hiccup-html'
+import { clipPolylinePoly } from '@thi.ng/geom-clip-line'
+
+const ROOT = document.getElementById('windowFrame'),
+    RES = [256, 256], // A resolution to sample the letters composition
+    THEME = ['steelblue', 'tomato', 'LimeGreen', 'gold', 'indigo'],
+    T = [
+        ...'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnpqrstuvwxyz.?,:!^=+/\\~#'
+    ] // An array of possible char used in the composition
+
+// We need these two elements:
+// `comp` to reuse the composition to export an SVG
+// `cnvs` (the canvas element) to export a JPG
+let comp = group(),
+    cnvs = null
+
+const init = () => {
+    // A canvas will be added to the DOM before init() happens,
+    // grab the element to draw the composition
+    cnvs = document.getElementById('main')
+    // Nothing fancy here we need a context to draw in the canvas
+    const ctx = cnvs.getContext('2d'),
+        // The dimension of the composition we need to remove 30 pixels of the
+        // window height to get room for the two download buttons
+        frame = [window.innerWidth * 0.8, window.innerHeight * 0.8],
+        // The diagonal will be use later to compute distance (with more flexibility)
+        diag = Math.hypot(...frame),
+        // A crop zone [[x, y], [width, height]
+        crop = [[diag * 0.01, diag * 0.01], frame.map((d) => d - diag * 0.02)],
+        // Same thing but with four points (clockwisely, from top left to bottom right)
+        cropPoly = [
+            [crop[0][0], crop[0][1]],
+            [crop[1][0], crop[0][1]],
+            [crop[1][0], crop[1][1]],
+            [crop[0][0], crop[1][1]]
+        ],
+        // How many chars we will put in the canvas
+        numChar = SYSTEM.minmaxInt(12, 36),
+        // Convert the number of chars to an array of chars
+        chars = [...repeatedly(() => pickRandom(T), numChar)],
+        // Then convert it to an array of lines
+        lines = chars.reduce((polys, char) => {
+            const size = Math.round(SYSTEM.minmax(0.1, 0.5) * diag)
+            const pos = frame.map((d) => d / 2 + SYSTEM.minmax(-0.35, 0.35) * d)
+            // Get a nested list of point from a char (grouped by lines)
+            // [
+            //  first line
+            //  [[x1, y1], [x2, y2]...],
+            //  second line
+            //  [[x1, y1], [x2, y2]...],
+            // ]
+            const letterLines = getGlyphVector(
+                // This function can be use with only one char
+                char,
+                // Fonts are designed in square of 0->1, you can adapt the ratio here
+                [size, size * 1.4],
+                // You can also add a position [x, y] or move the points obtained later [0, 0].
+                pos.map((d) => d - size / 2)
+            )
+            // Since a letter can contains multiple lines and we want a flat array,
+            // we use spread syntax and Array.reduce() to concatenate previous letter strokes with new ones
+            return [...polys, ...letterLines.map((line) => polyline(line))]
+        }, []),
+        // SDF generation with @thi.ng/geom-sdf
+        // I simply reuse the example of the repository (written by Karsten Schimdt)
+        // Some documentation are available here (and some examples too)
+        // https://github.com/thi-ng/umbrella/tree/develop/packages/geom-sdf
+        sdf = asSDF(group({}, lines)),
+        image = sample2d(sdf, rect(...crop), RES),
+        contours = asPolygons(
+            image,
+            rect(...crop),
+            RES,
+            range(diag * 0.003, diag, diag * 0.005),
+            0.5
+        ),
+        weight = diag * 0.0015
+    comp = group({}, [
+        group({}, [rect(frame, { fill: '#121010' })]),
+        group({ stroke: '#f3f6fa', weight }, contours),
+        group(
+            {
+                // fix a discrepancy whose origin I do not know
+                translate: [weight, weight],
+                lineJoin: 'round',
+                lineCap: 'round',
+                stroke: pickRandom(THEME),
+                weight
+            },
+            // since letter can be larger than sdf, cut lines if they go outside
+            lines.reduce(
+                (crp, l) => [
+                    ...crp,
+                    // Usefull function from @thi.ng/geom-clip-line,
+                    // It removes all parts of line which are outside a polygon
+                    // https://github.com/thi-ng/umbrella/tree/7d3339310c56ac7fd3572fb3487b6f34f1de57ca/packages/geom-clip-line#api
+                    ...clipPolylinePoly(l.points, cropPoly).map((pts) =>
+                        polyline(pts)
+                    )
+                ],
+                []
+            )
+        )
+    ])
+
+    cnvs.width = frame[0]
+    cnvs.height = frame[1]
+    draw(ctx, comp)
+}
+
+const downloadJpg = () =>
+    downloadCanvas(cnvs, `letter-offset-${FMT_yyyyMMdd_HHmmss()}`, 'jpeg', 1)
+
+const downloadSvg = () =>
+    downloadWithMime(
+        `letter-offset-${FMT_yyyyMMdd_HHmmss()}.svg`,
+        asSvg(
+            svgDoc(
+                {
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                    viewBox: `0 0 ${window.innerWidth} ${window.innerHeight}`
+                },
+                comp
+            )
+        ),
+        { mime: 'image/svg+xml' }
+    )
+
+$compile(canvas('#main')).mount(ROOT)
+
+/* stuff relative to forsaken-ideas */
+window.init = init
+window.downloadJpg = downloadJpg
+window.downloadSvg = downloadSvg
+infobox()
+handleAction()
+
+init()
---
+++ sketch/letter-to-geometry/index.js
@@ -0,0 +1,209 @@
+import '../full-canvas.css'
+import * as THREE from 'three'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { getGlyphVector } from '@nclslbrn/plot-writer'
+
+const T = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'] // An array of possible char used in the composition
+const d = 0.025
+const extruderPoints = [
+    [-d, -d],
+    [d, -d],
+    [d, d],
+    [-d, d]
+]
+const extrudesShape = new THREE.Shape()
+extrudesShape.moveTo(...extruderPoints[0], 0)
+extruderPoints.forEach((pt) => extrudesShape.lineTo(...pt, 0))
+
+const windowFrame = document.getElementById('windowFrame')
+
+const glyphGeom = (letter, size) => {
+    const glyph = getGlyphVector(letter, size, [-size[0] / 2, -size[1] / 2])
+    const closedSplines = glyph.map((l) => {
+        const unloop = l[0] === l[l.length - 1] ? l.slice(0, -1) : [...l]
+        const spline = new THREE.CatmullRomCurve3([
+            ...unloop.map((p) => new THREE.Vector3(...p, 0))
+        ])
+        if (l[0] === l[l.length - 1]) {
+            spline.closed = true
+        } else {
+            spline.closed = false
+        }
+        spline.curveType = 'catmullrom'
+        spline.tension = 0
+        return spline
+    })
+
+    return closedSplines.map((spl) => {
+        const extrusion = {
+            steps: spl.arcLengthDivisions,
+            bevelEnabled: false,
+            extrudePath: spl
+        }
+        return new THREE.ExtrudeGeometry(extrudesShape, extrusion)
+    })
+}
+
+class App {
+    constructor() {
+        this.initScene()
+        this.initStats()
+        this.initListeners()
+    }
+
+    initStats() {
+        this.stats = new Stats()
+        windowFrame.appendChild(this.stats.dom)
+        this.stats.dom.style.top = 'auto'
+        this.stats.dom.style.left = 'auto'
+        this.stats.dom.style.right = '0'
+        this.stats.dom.style.bottom = '0'
+    }
+
+    createGlyph(letter, pos, rx, ry) {
+        const glyphGeometry = glyphGeom(letter, [0.4, 1])
+        glyphGeometry.map((geom) => {
+            const m = new THREE.Mesh(geom, this.glyphMat)
+            m.position.x = pos[0]
+            m.position.y = pos[1]
+            m.position.z = pos[2] + 0.25
+            m.rotation.y = ry
+            m.rotation.x = rx
+            m.castShadow = true
+            m.receiveShadow = true
+            this.scene.add(m)
+            this.glyphs.add(m)
+        })
+    }
+
+    initScene() {
+        this.scene = new THREE.Scene()
+        this.glyphMat = new THREE.MeshLambertMaterial({ color: 0xffffff })
+        this.camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            20
+        )
+        this.camera.position.y = -4
+        this.camera.position.z = 2
+        this.camera.lookAt(this.scene.position)
+
+        this.renderer = new THREE.WebGLRenderer()
+        this.renderer.shadowMap.enabled = true
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
+        this.renderer.setPixelRatio(window.devicePixelRatio)
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+
+        windowFrame.appendChild(this.renderer.domElement)
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+        this.lightAmbient = new THREE.AmbientLight(0x222222)
+        this.scene.add(this.lightAmbient)
+
+        // Add a point light to add shadows
+        // https://github.com/mrdoob/three.js/pull/14087#issuecomment-431003830
+        const shadowIntensity = 1
+
+        this.lightPoint = new THREE.PointLight(0xffffff)
+        this.lightPoint.position.set(0, 0, 3)
+        this.lightPoint.castShadow = true
+        this.lightPoint.intensity = shadowIntensity
+        this.scene.add(this.lightPoint)
+
+        const lightPoint2 = this.lightPoint.clone()
+        lightPoint2.intensity = 1 - shadowIntensity
+        lightPoint2.castShadow = true
+        this.scene.add(lightPoint2)
+
+        const mapSize = 1024 // Default 512
+        const cameraNear = 0.5 // Default 0.5
+        const cameraFar = 500 // Default 500
+        this.lightPoint.shadow.mapSize.width = mapSize
+        this.lightPoint.shadow.mapSize.height = mapSize
+        this.lightPoint.shadow.camera.near = cameraNear
+        this.lightPoint.shadow.camera.far = cameraFar
+
+        this.glyphs = new THREE.Group()
+        for (let z = 0; z <= 2; z++) {
+            for (let y = -2; y <= 2; y++) {
+                for (let x = -2; x <= 2; x++) {
+                    this.createGlyph(
+                        T[Math.ceil(Math.random() * T.length)],
+                        [x * 0.35, y * 0.5, z * 0.5],
+                        -Math.PI / 2,
+                        (x + z) % 2 === 0 ? Math.PI / 4 : -Math.PI / 4
+                    )
+                    this.createGlyph(
+                        T[Math.ceil(Math.random() * T.length)],
+                        [x * 0.35, y * 0.5, z * 0.5],
+                        0,
+                        0
+                    )
+                }
+            }
+        }
+        this.glyphs.position.z = 0
+        this.scene.add(this.glyphs)
+
+        // Add a plane
+        const geometryPlane = new THREE.PlaneGeometry(
+            window.innerWidth / 60,
+            window.innerHeight / 60,
+            1,
+            1
+        )
+        const materialPlane = new THREE.MeshPhongMaterial({ color: 0xcccccc })
+
+        this.plane = new THREE.Mesh(geometryPlane, materialPlane)
+        this.plane.position.z = 0
+        this.plane.receiveShadow = true
+        this.scene.add(this.plane)
+
+        // Init animation
+        this.animate()
+    }
+
+    initListeners() {
+        window.addEventListener('resize', this.onWindowResize.bind(this), false)
+        window.addEventListener('keydown', (event) => {
+            const { key } = event
+            switch (key) {
+                case 'e':
+                    const win = window.open('', 'Canvas Image')
+                    const { domElement } = this.renderer
+                    // Makse sure scene is rendered.
+                    this.renderer.render(this.scene, this.camera)
+                    const src = domElement.toDataURL()
+                    if (!win) return
+                    win.document.write(
+                        `<img src='${src}' width='${domElement.width}' height='${domElement.height}'>`
+                    )
+                    break
+                default:
+                    break
+            }
+        })
+    }
+
+    onWindowResize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight
+        this.camera.updateProjectionMatrix()
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+    }
+
+    animate() {
+        requestAnimationFrame(() => {
+            this.animate()
+        })
+        this.glyphs.rotation.z += 0.005
+
+        if (this.stats) this.stats.update()
+        if (this.controls) this.controls.update()
+        this.renderer.render(this.scene, this.camera)
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    new App()
+})
