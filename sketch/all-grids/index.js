59c4ec89ba3ad5e0dc43d8ab81964c37394b1f1d
nclslbrn <nclslbrn@gmail.com>
2024-06-07 20:09:21 +0200

    unfinished

--- sketch/all-grids/index.js
+++ sketch/all-grids/index.js
@@ -11,35 +11,52 @@ import { SYSTEM, pickRandom } from '@thi.ng/random'
 import { $compile } from '@thi.ng/rdom'
 import { canvas } from '@thi.ng/hiccup-html'
 import { adaptDPI, isHighDPI } from '@thi.ng/canvas'
-
-const ROOT = document.getElementById('windowFrame')
+const ROOT = document.getElementById('windowFrame'),
+    MUL = 0.66 //(1 + Math.sqrt(5)) / 4
 
 let comp = group(),
     cnvs = null,
     grids = []
 
-const splitCell = (num) => {
-    const toSplit = num % grids[num].length
-    const [x, y, w, h] = grids[num][toSplit]
-    const splitOn = num % 2 === 0 ? 'x' : 'y'
+const splitCell = (x, y, g, i, variation) => {
+    // Get the last or penultimate cell ID
+    const splitOn = (g + (x % i)) % variation < variation / 2 ? 'x' : 'y'
+    const splitAt = (g + (y % i)) % 2 === 0 ? MUL : 1 - MUL
+    const storeRev = (g * i) % 4 === 0
+
+    const toSplit = 0 //grids[g].length - 1
+    console.log(toSplit)
+    // Get cell to divide property
+    console.log(grids.length, g)
+    const [cx, cy, cw, ch] = grids[g][toSplit]
+    // Based on current iteration choose how to split cell
+
     const splitted = []
     if (splitOn === 'x') {
         splitted.push(
             ...[
-                [x, y, w * 0.5, h],
-                [x + w * 0.5, y, w * 0.5, h]
+                [cx, cy, cw * splitAt, ch],
+                [cx + cw * splitAt, cy, cw * (1 - splitAt), ch]
             ]
         )
     } else {
         splitted.push(
             ...[
-                [x, y, w, h * 0.5],
-                [x, y + h * 0.5, w, h * 0.5]
+                [cx, cy, cw, ch * splitAt],
+                [cx, cy + ch * splitAt, cw, ch * (1 - splitAt)]
             ]
         )
     }
-    grids[num].splice(toSplit, 1)
-    grids[num].push(...splitted)
+
+    // console.log(`(${x}, ${y}, ${g}/${grids.length}, ${i}) => [${toSplit}] / ${splitOn}`)
+
+    grids[g].splice(toSplit, 1)
+    // Based on current grid  choose how to store cells
+    if (storeRev) {
+        grids[g].push(...splitted.reverse())
+    } else {
+        grids[g].push(...splitted)
+    }
 }
 
 const init = () => {
@@ -47,39 +64,52 @@ const init = () => {
     // Nothing fancy here we need a context to draw in the canvas
     const ctx = cnvs.getContext('2d')
     const dpr = adaptDPI(cnvs, window.innerWidth, window.innerHeight)
-    const numGrid = 6
+    const variation = 2 // depends of splitCell choices
     const between = 20
     const width = window.innerWidth * dpr
     const height = window.innerHeight * dpr
-
-    grids = [...repeatedly(() => [[0, 0, 1, 1]], numGrid * numGrid)]
-    //console.log(grids.length, numGrid * numGrid)
-    for (let i = 0; i < numGrid * numGrid; i++) {
-        splitCell(i)
+    const numGrids = variation * 4
+    const rowCol = variation * variation
+    // init all grid with the first cell (full size)
+    grids = [...repeatedly(() => [[0, 0, 1, 1]], numGrids * 2)]
+    console.log(grids.length)
+    for (let y = 0; y < rowCol; y++) {
+        for (let x = 0; x < rowCol; x++) {
+            for (let j = 0; j < variation; j++) {
+                splitCell(x, y, y * rowCol + x, j, numGrids)
+            }
+        }
     }
-    const gridWidth = width - (between * 2) / numGrid - between * numGrid
-    const gridHeight = height - (between * 2) / numGrid - between * numGrid
-    const cells = []
+    const gridWidth = (width - between - between * rowCol) / rowCol
+    const gridHeight = (height - between - between * rowCol) / rowCol
+    const cells = [],
+        numbers = []
 
-    for (let y = 0; y < numGrid; y++) {
-        for (let x = 0; x < numGrid; x++) {
-            const cId = y * numGrid + x
+    for (let y = 0; y < rowCol; y++) {
+        for (let x = 0; x < rowCol; x++) {
+            const cId = y * rowCol + x
             const dx = between + x * (gridWidth + between)
             const dy = between + y * (gridHeight + between)
             for (let c = 0; c < grids[cId].length; c++) {
                 const [x, y, w, h] = grids[cId][c]
-                cells.push(
-                    rect(
-                        [dx + x * gridWidth, dy + y * gridHeight],
-                        [w * gridWidth, h * gridHeight]
-                    )
+                const pos = [dx + x * gridWidth, dy + y * gridHeight],
+                    siz = [w * gridWidth, h * gridHeight],
+                    mSz = 64
+                cells.push(rect(pos, siz))
+                numbers.push(
+                    ...getGlyphVector(
+                        String(c),
+                        [mSz / 2, mSz / 2],
+                        pos.map((d, i) => d + siz[i] / 2 - mSz / 4)
+                    ).map((l) => polyline(l))
                 )
             }
         }
     }
     comp = group({ stroke: '#333' }, [
         rect([width, height], { fill: '#fff' }),
-        ...cells
+        group({ fill: '#00000000' }, cells),
+        group({}, numbers)
     ])
     draw(ctx, comp)
 }
