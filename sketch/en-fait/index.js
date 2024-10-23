4f96a7e40dc3c4b6c6ed088294b0ce9bfc30bd1e
nclslbrn <nclslbrn@gmail.com>
2024-10-23 20:00:26 +0200

    add easing (in rotation)

--- sketch/en-fait/index.js
+++ sketch/en-fait/index.js
@@ -1,4 +1,4 @@
-import { rect, polyline, group, svgDoc } from '@thi.ng/geom'
+import { rect, polyline, group, translate, rotate, svgDoc } from '@thi.ng/geom'
 import { pickRandom } from '@thi.ng/random'
 import { FMT_yyyyMMdd_HHmmss } from '@thi.ng/date'
 import '../full-canvas.css'
@@ -11,7 +11,8 @@ import {
 } from '@thi.ng/dl-asset'
 import { getParagraphVector } from '@nclslbrn/plot-writer'
 import { draw } from '@thi.ng/hiccup-canvas'
-import { convert, mul, quantity, NONE, mm, dpi, DIN_A3 } from '@thi.ng/units'
+import remap from '../../sketch-common/remap'
+import ease from '../../sketch-common/ease'
 import SENTENCES from './johnfluo-text'
 
 const ROOT = document.getElementById('windowFrame'),
@@ -20,7 +21,10 @@ const ROOT = document.getElementById('windowFrame'),
     SIZE = [1920, 1080],
     MARGIN = 300,
     PRE = 'En fait ',
-    LETTER_TIME = 30
+    LETTER_TIME = 50,
+    TRANSITION = 3000,
+    STR_ON_SCREEN = 7,
+    COLOR = ['tomato', 'steelblue', 'limegreen', 'indigo', 'gold']
 
 let currSentence = 0,
     width,
@@ -30,8 +34,6 @@ let currSentence = 0,
     prevDrawTime = performance.now()
 
 ROOT.appendChild(CANVAS)
-const randColor = () =>
-    pickRandom(['tomato', 'steelblue', 'limegreen', 'indigo', 'gold'])
 
 const init = () => {
     width = SIZE[0]
@@ -43,34 +45,80 @@ const init = () => {
 
 const update = () => {
     if (currSentence < SENTENCES.length - 1) {
+        let t = 0
         frameReqest = requestAnimationFrame(update)
+        const readTime = SENTENCES[currSentence].length * LETTER_TIME
         //console.log(`${prevDrawTime} - ${performance.now()}`)
-        if (
-            performance.now() - prevDrawTime >=
-            SENTENCES[currSentence].length * LETTER_TIME
-        ) {
+        if (performance.now() - prevDrawTime >= readTime + TRANSITION) {
             currSentence++
             prevDrawTime = performance.now()
         }
 
-        const letters = getParagraphVector(
-            PRE + SENTENCES[currSentence],
-            24,
-            5,
-            SIZE[0] - MARGIN * 2
-        )
-
-        drawElems = [
-            rect(SIZE, { fill: randColor() }),
-            rect([width, height], { fill: '#111' }),
-            group(
-                { lineCap: 'round', weight: 2, stroke: 'white' },
-                letters.vectors.reduce((polys, group) => [
-                    ...polys,
-                    ...group.map((line) => polyline(line))
-                ], [])
+        if (performance.now() - prevDrawTime >= readTime) {
+            t = ease(
+                remap(
+                    performance.now() - (prevDrawTime + readTime),
+                    0,
+                    TRANSITION,
+                    0,
+                    1
+                ),
+                15
             )
-        ]
+        }
+
+        drawElems = [rect([width, height], { fill: '#111' })]
+
+        for (let i = -STR_ON_SCREEN; i <= 0; i++) {
+            if (currSentence + i >= 0) {
+                const str = PRE + SENTENCES[currSentence + i]
+                const letters = getParagraphVector(
+                    str,
+                    26,
+                    10,
+                    SIZE[0] - MARGIN * 4,
+                    [1, 0.8]
+                )
+
+                drawElems.push(
+                    translate(
+                        rotate(
+                            translate(
+                                group(
+                                    {
+                                        lineCap: 'round',
+                                        weight: 2,
+                                        stroke: 'white'
+                                    },
+                                    [
+                                        polyline(
+                                            [
+                                                [0, -16],
+                                                [120, -16]
+                                            ],
+                                            { weight: 4 }
+                                        ),
+                                        ...letters.vectors.reduce(
+                                            (polys, group) => [
+                                                ...polys,
+                                                ...group.map((line) =>
+                                                    polyline(line)
+                                                )
+                                            ],
+                                            []
+                                        )
+                                    ]
+                                ),
+                                [MARGIN * (i === 0 ? 2 : 0.3), 0]
+                            ),
+                            (-i / 4) * Math.PI +
+                                (i === 0 ? 0 : (t * Math.PI) / 4)
+                        ),
+                        [MARGIN * 2, MARGIN * (i === 0 ? 0.6 : 1.8)]
+                    )
+                )
+            }
+        }
     }
     draw(CTX, group({}, drawElems))
 }
