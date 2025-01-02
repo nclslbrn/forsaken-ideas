df37fe39c2f6ef6a3f9278bad210554191c27b9c
nclslbrn <nclslbrn@gmail.com>
2025-01-02 12:06:18 +0100

    prevent building script using a mysterious node_modules folder as sketch/project entry point

--- tasks/concat-sketch-properties.mjs
+++ tasks/concat-sketch-properties.mjs
@@ -1,31 +1,35 @@
-import path from 'path';
-import fs from 'fs';
-import titleFromSlug from './utils/title-from-slug.mjs';
-import readJSON from './utils/read-json.mjs';
-import fileList from './utils/file-list.mjs';
+import path from 'path'
+import fs from 'fs'
+import titleFromSlug from './utils/title-from-slug.mjs'
+import readJSON from './utils/read-json.mjs'
+import fileList from './utils/file-list.mjs'
 
-const exportCallBack = (err) =>
-  console.log(err ? err : 'Sketch list exported')
+const exportCallBack = (err) => console.log(err ? err : 'Sketch list exported')
 
-const exportSketches = (sketchProperties) => fs.writeFile(
-  path.resolve('./public/sketch/index.json'),
-  JSON.stringify(sketchProperties),
-  'utf8',
-  exportCallBack
-);
+const exportSketches = (sketchProperties) =>
+    fs.writeFile(
+        path.resolve('./public/sketch/index.json'),
+        JSON.stringify(sketchProperties),
+        'utf8',
+        exportCallBack
+    )
 
-const projects = fileList(path.resolve('./sketch/'));
+const projects = fileList(path.resolve('./sketch/'))
 
 const sketchProperties = []
 
 for (let i = 0; i < projects.length; i++) {
-  const project = await readJSON(path.resolve(`./sketch/${projects[i]}/property.json`))
-  sketchProperties.push({
-    title: titleFromSlug(projects[i]),
-    topic: project.topic,
-    date: project.date,
-    src: projects[i]
-  })
+    if (projects[i] !== 'node_modules') {
+        const project = await readJSON(
+            path.resolve(`./sketch/${projects[i]}/property.json`)
+        )
+        sketchProperties.push({
+            title: titleFromSlug(projects[i]),
+            topic: project.topic,
+            date: project.date,
+            src: projects[i]
+        })
+    }
 }
 
-exportSketches(sketchProperties);
+exportSketches(sketchProperties)
