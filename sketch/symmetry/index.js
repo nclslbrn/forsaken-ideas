b17afc97ae7f0d878d739ef4d9310228ff20f41c
nclslbrn <nclslbrn@gmail.com>
2025-01-30 21:52:59 +0100

    clean unused functions

--- sketch/symmetry/index.js
+++ sketch/symmetry/index.js
@@ -3,34 +3,12 @@ import infobox from '../../sketch-common/infobox'
 import handleAction from '../../sketch-common/handle-action'
 import vertSrc from './glsl/base.vert'
 import fragSrc from './glsl/base.frag'
-import { canvasRecorder } from '@thi.ng/dl-asset'
-
-let isRecording = false,
-    recorder = false
 
 const capture = (canvas) => {
     const link = document.createElement('a')
     link.download = `symmetry.jpg`
     link.href = canvas.toDataURL('image/jpg')
     link.click()
-}
-
-const startRecording = (canvas) => {
-    if (isRecording) return
-    recorder = canvasRecorder(canvas, 'symmetry', {
-        mimeType: 'video/webm;codecs=vp8',
-        fps: 30
-    })
-    recorder.start()
-    console.log('%c Record started ', 'background: tomato; color: white')
-    isRecording = true
-}
-
-const stopRecording = () => {
-    if (!isRecording) return
-    recorder.stop()
-    console.log('%c Record stopped ', 'background: limegreen; color: black')
-    isRecording = false
 }
 
 const createShader = (gl, type, source) => {
@@ -67,7 +45,7 @@ const setup = () => {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
     gl.viewport(0, 0, canvas.width, canvas.height)
-    // Create shaders and program
+
     const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertSrc),
         fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragSrc),
         program = createProgram(gl, vertexShader, fragmentShader)
@@ -112,11 +90,4 @@ containerElement.appendChild(canvas)
 setup()
 window.infobox = infobox
 window.capture = () => capture(canvas)
-window.init = () => console.log('no init')
-window.onkeydown = (e) => {
-    if (e.key.toLowerCase() === 'r') startRecording(canvas)
-    if (e.key.toLowerCase() === 's') stopRecording()
-    if (e.key.toLowerCase() === 'g') init()
-}
-
 handleAction()
