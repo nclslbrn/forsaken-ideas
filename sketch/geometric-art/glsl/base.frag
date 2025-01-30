bfa6c6271be7de640a63fe737f46db8321e5ecca
nclslbrn <nclslbrn@gmail.com>
2025-01-30 10:44:11 +0100

    update geometric-art (complete change)

--- sketch/geometric-art/glsl/base.frag
+++ sketch/geometric-art/glsl/base.frag
@@ -1,20 +1,58 @@
-precision lowp float;
 
-#define MAX_RHOMB 128
-#define PI 3.1415926535897
+precision highp float;
+
+#define MAX_CELL 64.0
+#define PI 4.
 
 uniform vec2 u_resolution;
+uniform int u_numCell;
+uniform vec4 u_cells[int(MAX_CELL)];
+uniform float u_noiseSize;
+uniform float u_noiseSeed;
 uniform float u_time;
-// By Inigo Quilez
-// https://iquilezles.org/articles/distfunctions2d/
-float ndot(vec2 a, vec2 b) {
-    return a.x * b.x - a.y * b.y;
+
+vec2 hash22(vec2 p, float seed) {
+    vec3 p3 = fract(vec3(p.xyx) * vec3(.1031, .1030, .0973));
+    p3 += dot(p3, p3.yzx + 33.33 + seed);
+    return fract((p3.xx + p3.yz) * p3.zy);
 }
-float sdCircle( vec2 p, float r ){
+
+float smooth_noise(vec2 p, float seed) {
+    vec2 i = floor(p);
+    vec2 f = fract(p);
+    float a = hash22(i, seed).x;
+    float b = hash22(i + vec2(1.0, 0.0), seed).x;
+    float c = hash22(i + vec2(0.0, 1.0), seed).x;
+    float d = hash22(i + vec2(1.0, 1.0), seed).x;
+    vec2 u = f * f * (3.0 - 2.0 * f);
+    return mix(
+        mix(a, b, u.x),
+        mix(c, d, u.x),
+        u.y
+    );
+}
+
+float noise2D(vec2 p, float seed) {
+    float value = 0.0;
+    float amplitude = 0.5;
+    float frequency = 1.0;
+    for (int i = 0; i < 4; i++) {
+        value += smooth_noise(p * frequency, seed) * amplitude;
+        amplitude *= 0.5;
+        frequency *= 2.0;
+    }
+    return value;
+}
+
+float sdBox(in vec2 p, in vec2 wh) {
+    vec2 d = abs(p) - wh;
+    return length(max(d, 0.0)) + min(max(d.x, d.y), 0.0);
+}
+
+float sdCircle(vec2 p, float r) {
     return length(p) - r;
 }
 
-// Unidentified source, probably Piter Pasma
 float sdfRep(in float x, in float r) {
     x /= r;
     x -= floor(x) + .5;
@@ -22,23 +60,51 @@ float sdfRep(in float x, in float r) {
     return x;
 }
 
+vec2 rotate2D(vec2 _st, float _angle) {
+    _st -= 0.5;
+    _st = mat2(cos(_angle), -sin(_angle),
+            sin(_angle), cos(_angle)) * _st;
+    _st += 0.5;
+    return _st;
+}
+
 void main() {
     vec2 st = gl_FragCoord.xy / u_resolution.xy;
-    vec2 bl = step(vec2(0.05), st);
-    vec2 tr = step(vec2(0.05), 1.0 - st);
-    float wheight = .08;
-    float rr = smoothstep(0., .5, mod(u_time, 1.));
-    float d = abs(sdfRep(sdCircle(vec2(.5)-st,.3),.03)-.06);
-    if (d < wheight) {
-      float bl = abs(sdfRep(sdCircle(vec2(.125)-st*2., rr), .05) -.07);
-      float br =  abs(sdfRep(sdCircle(vec2(1.875,.125)-st*2., rr), .05) -.07); 
-      float tr =  abs(sdfRep(sdCircle(vec2(1.875)-st*2., rr), .05) -.07);
-      float tl =  abs(sdfRep(sdCircle(vec2(.125,1.875)-st*2., rr), .05) -.07); 
-      d = max(max(bl, br), max(tl, tr));
-    } 
-    float fill = 1.-(bl.x * bl.y * tr.x * tr.y);
-    float stroke = d < wheight && d > -wheight ? 0. : 1.;
-    fill = max(fill, stroke);
+    st *= 1.2;
 
-    gl_FragColor = vec4(vec3(fill), 1.0);
+    vec3 color = vec3(.94, .90, .93) - noise2D(st*300., u_noiseSeed)*.05;
+
+    for (int i = 0; i <= int(MAX_CELL); i++) {
+        if (i < u_numCell) {
+            vec2 cellPos = vec2(0.1) + vec2(u_cells[i].xy);
+            vec2 cellSiz = vec2(u_cells[i].zw);
+            vec2 stToCell = st - cellPos;
+            int j = int(mod(float(i), 4.));
+            if (abs(st.y - cellPos.y) <= cellSiz.y * .5) {
+                if (abs(st.x - cellPos.x) <= cellSiz.x * .5) {
+                    if (j == 0) {
+                      stToCell -= cellSiz;
+                    } 
+                    if (j == 1) {
+                        stToCell.x += cellSiz.x * 2.;
+                    }
+                    if (j == 2) {
+                        stToCell += cellSiz * 2.;
+                    }
+                    if (j == 3) {
+                        stToCell.y += cellSiz.y * 2.;
+                    }
+                    float d = sdCircle(
+                            stToCell,
+                            // min(cellSiz.x, cellSiz.y)
+                            (mod(float(i), 2.) > 0. ? cellSiz.x : cellSiz.y) * .5
+                    );
+                    float rep = abs(sdfRep(d, .33) - .66);
+                    if (rep < .66) color *= 0.2;
+                }
+            }
+        }
+    }
+
+    gl_FragColor = vec4(color, 1.0);
 }
--- sketch/geometric-art/index.js
+++ sketch/geometric-art/index.js
@@ -6,8 +6,35 @@ import fragSrc from './glsl/base.frag'
 import { canvasRecorder } from '@thi.ng/dl-asset'
 
 let isRecording = false,
-    recorder = false
+    recorder = false,
+    traits = {}
 
+const { floor, ceil, random } = Math
+
+const shuffle = (array) => array.sort(() => 0.5 - random())
+
+const splitCell = (cellIdx, isHorizontal, grid) => {
+    if (grid[cellIdx] === undefined) return grid
+    const [x, y, w, h] = grid[cellIdx]
+    const c = 1 / ceil(1 + random() * 2)
+    let splitted = []
+    if (isHorizontal) {
+        const ws = shuffle([w * c, w * (1 - c)])
+        splitted = [
+            [x - w * 0.5 + ws[0] * 0.5, y, ws[0], h],
+            [x + w * 0.5 - ws[1] * 0.5, y, ws[1], h]
+        ]
+    } else {
+        const hs = shuffle([h * c, h * (1 - c)])
+        splitted = [
+            [x, y - h * 0.5 + hs[0] * 0.5, w, hs[0]],
+            [x, y + h * 0.5 - hs[1] * 0.5, w, hs[1]]
+        ]
+    }
+    grid.splice(cellIdx, 1)
+    grid.push(...splitted)
+    return grid
+}
 const capture = (canvas) => {
     const link = document.createElement('a')
     link.download = `geometric-art.jpg`
@@ -17,10 +44,14 @@ const capture = (canvas) => {
 
 const startRecording = (canvas) => {
     if (isRecording) return
-    recorder = canvasRecorder(canvas, 'geometric-art', {
-        mimeType: 'video/webm;codecs=vp8',
-        fps: 30
-    })
+    recorder = canvasRecorder(
+        canvas,
+        'geometric-art',
+        {
+            mimeType: 'video/webm;codecs=vp8',
+            fps: 30
+        }
+    )
     recorder.start()
     console.log('%c Record started ', 'background: tomato; color: white')
     isRecording = true
@@ -57,48 +88,83 @@ const createProgram = (gl, vertexShader, fragmentShader) => {
     return program
 }
 
-const setup = () => {
-    if (!gl) {
-        console.error('WebGL not supported')
-        return
-    }
+const sketch = {
+    frame: 0,
+    init: () => {
+        const numCell = 4 + ceil(random() * 12)
+        let cells = [[0.5, 0.5, 1, 1]]
 
-    canvas.width = 1920
-    canvas.height = 1920
-    gl.viewport(0, 0, canvas.width, canvas.height)
-    // Create shaders and program
-    const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertSrc),
-        fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragSrc),
-        program = createProgram(gl, vertexShader, fragmentShader)
+        for (let i = 0; i < numCell; i++)
+            cells = splitCell(
+                floor(random() * cells.length),
+                i % 2 === 0,
+                cells
+            )
 
-    gl.useProgram(program)
+        traits = {
+            noiseSeed: random() * 999,
+            noiseSize: 1 + random(),
+            numCell,
+            cells
+        }
+        sketch.render()
+    },
+    setup: () => {
+        if (!gl) {
+            console.error('WebGL not supported')
+            return
+        }
 
-    const buffer = gl.createBuffer(),
-        verts = new Float32Array([-1, -1, 1, -1, -1, 1, 1, 1]),
-        positionLoc = gl.getAttribLocation(program, 'a_position'),
-        uTimeLoc = gl.getUniformLocation(program, 'u_time'),
-        uResolution = gl.getUniformLocation(program, 'u_resolution')
+        canvas.width = 2400
+        canvas.height = 2400
+        gl.viewport(0, 0, canvas.width, canvas.height)
+        // Create shaders and program
+        const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertSrc),
+            fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragSrc),
+            program = createProgram(gl, vertexShader, fragmentShader)
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-    gl.bufferData(gl.ARRAY_BUFFER, verts, gl.STATIC_DRAW)
-    gl.enableVertexAttribArray(positionLoc)
-    gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0)
-    gl.disable(gl.DEPTH_TEST)
-    gl.enable(gl.BLEND)
-    let frame = 0
+        gl.useProgram(program)
 
-    const render = () => {
+        const buffer = gl.createBuffer(),
+            verts = new Float32Array([-1, -1, 1, -1, -1, 1, 1, 1]),
+            positionLoc = gl.getAttribLocation(program, 'a_position')
+
+        sketch.uTimeLoc = gl.getUniformLocation(program, 'u_time'),
+        sketch.uResolution = gl.getUniformLocation(program, 'u_resolution'),
+        sketch.uNoiseSeed = gl.getUniformLocation(program, 'u_noiseSeed'),
+        sketch.uNoiseSize = gl.getUniformLocation(program, 'u_noiseSize'),
+        sketch.uNumCell = gl.getUniformLocation(program, 'u_numCell'),
+        sketch.uCells = gl.getUniformLocation(program, 'u_cells')
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+        gl.bufferData(gl.ARRAY_BUFFER, verts, gl.STATIC_DRAW)
+        gl.enableVertexAttribArray(positionLoc)
+        gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0)
+        gl.disable(gl.DEPTH_TEST)
+        gl.enable(gl.BLEND)
+        sketch.init()
+        //startRecording(canvas)
+
+        gl.clearColor(1, 1, 1, 1)
+        sketch.render()
+    },
+    render: () => {
+        //frame === 100 && stopRecording()
         gl.clear(gl.COLOR_BUFFER_BIT)
-        gl.uniform1f(uTimeLoc, frame * 0.0001)
-        gl.uniform2f(uResolution, canvas.width, canvas.height)
+        gl.uniform1f(sketch.uTimeLoc, sketch.frame * 0.01)
+        gl.uniform2f(sketch.uResolution, canvas.width, canvas.height)
+        gl.uniform1f(sketch.uNoiseSeed, traits.noiseSeed)
+        gl.uniform1f(sketch.uNoiseSize, traits.noiseSize)
+        gl.uniform1i(sketch.uNumCell, traits.numCell)
+        gl.uniform4fv(
+            sketch.uCells,
+            traits.cells.reduce((s, c) => [...s, ...c], [])
+        )
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
 
-        frame++
-        requestAnimationFrame(render)
-        
+        sketch.frame++
+        //requestAnimationFrame(sketch.render)
     }
-    gl.clearColor(1, 1, 1, 1)
-    render()
 }
 
 const containerElement = document.getElementById('windowFrame'),
@@ -108,8 +174,9 @@ const containerElement = document.getElementById('windowFrame'),
 
 containerElement.removeChild(loader)
 containerElement.appendChild(canvas)
-setup()
+sketch.setup()
 window.infobox = infobox
+window.randomize = sketch.init
 window.capture = () => capture(canvas)
 
 window.onkeydown = (e) => {
