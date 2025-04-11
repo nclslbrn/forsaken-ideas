c26dd4c3b3b9ace3020481afbc4bb7a5ed60978b
nclslbrn <nclslbrn@gmail.com>
2025-04-11 19:39:47 +0200

    use random sentences instead of random words

--- sketch/book-cover/index.js
+++ sketch/book-cover/index.js
@@ -9,7 +9,7 @@ const ROOT = document.getElementById('windowFrame'),
     MARGIN = 200,
     SVG = new SvgTracer({
         parentElem: ROOT,
-        size: 'A3_portrait',
+        size: 'A4_portrait', //{ w: 45.5, h: 27 },
         background: '#fff',
         dpi: 300
     })
@@ -24,28 +24,43 @@ const addFontFile = () => {
     style.sheet.insertRule(`text { font-weight: 900; }`, style.sheet.cssRules.length)
 
 }
-const texts = [
+const sentences = [
+    'A protocol creating arbitrary composition with randomnes using a small range of geometric elements.',
+    'The program use recursion as if it were a self-reactive process.',
+    'As an artist, I create a protocol to generate a composition but because of randomness, I also become a spectator when my program runs again, there is something unexpected.',
+    'Coder artists work in this back and fourth, by changing the process and watching what it can produce.',
+    'You have to leave enough choice to randomness, enough entropy, so that you can imagine what the process will produce, but without being completely sure.',
+    'We may be seduced by a particular edition of a generative series, but we\'ll have a better understanding of the protocol when we look at all the editions produced.',
+    'It was an opportunity to introduce new rules into the protocol.',
+    'Each project is conceived as a system, with an initial state and a process that modifies this state to stop and freeze the composition.',
+    'While the result of this protocol may evoke architectural, modern, or futuristic forms, it is fundamentally a process of deconstruction and accumulation-driven by an absurd mechanism.',
+
+/*
     'iteration', 'alteration', 'recursion', 'collision', 'constraint', 'composition', 'protocol', 'system', 'grids', 'point',
     'organization', 'spacing', 'shearing', 'possible', 'squares', 'transformation', 'balance', 'precision', 'radicalism',
     'condition', 'rhythm', 'tangible', 'infra-thin', 'sequences', 'language', 'deconstruction', 'abstraction', 'strata', 'manipulation', 
     'architectural', 'process', 'mechanism', 'accumulation', 'repetition', 'intricate', 'opposing', 'combining', 'singularity', 'self-reflective',
     'successive', 'section', 'shape', 'phenomenon', 'replacing', 'arbitrary', 'synthetic', 'superimposing', 'groups', 'distances', 
     'hatchings', 'instructions', 'algorithms', 'coherent', 'polygon', 'parallel', 'stacked', 'subdivided', 'experiments', 'decompose', 'rules',
-    'straight', 'perpendicular', 'perspective', 'geometry', 
+    'straight', 'perpendicular', 'perspective', 'geometry', 'entropy', 
     
-    'while', 'whatever', 'no more', 'if',
-    '+', '|', '_____', '-------->', '<----', ">>>", '^^^', '?', '******', '** ***', '[error]', '&'
+    'while', 'whatever', 'no more', 'if', 'from', 'for', 'then', 'against', 
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    '+', '|', '_____', '-------->', '<----', ">>>", '^^^', '******', '** ***', '[error]', '&'
+*/
 ]
+
 
 const sketch = {
     init: () => {
         const inner = [SVG.width - MARGIN*2, SVG.height - MARGIN*2],
             base = 64,
             svgDisplayWidth = SVG.elem.getBoundingClientRect().width,
+            texts = [...sentences[Math.floor(Math.random() * sentences.length)].split(' ')],
             palette = getPalette();
 
         let y = MARGIN, 
-            fontSize = base
+            fontSize = base * 1.33 * Math.ceil(Math.random() * 3)
         
         SVG.clear()
         SVG.rect({ 
@@ -57,13 +72,18 @@ const sketch = {
         })
         let w = 0, l = 0;
 
-        while (y + fontSize * 3  < inner[1]) {
+        while (y + fontSize  < inner[1]) {
             y += fontSize * (fontSize === base ? 3 : 1.25)
             let x  = MARGIN
 
-            while (x < inner[0] - MARGIN * 3) { 
-                const text = texts[Math.floor(Math.random() * texts.length)]
-                if (x + text.length * fontSize * 0.5 < inner[0]) { 
+            while (
+                x < inner[0] - MARGIN * 3 // check if enough place 
+                || (x > inner[0] && texts.length - (w % texts.length) < 1) // or if the sentence will end soon
+            ) { 
+                const text = texts[w % texts.length]
+                if (
+                    x + text.length * fontSize * 0.5 < inner[0] // check for x overflow
+                ) { 
                     SVG.text({
                         x, 
                         y, 
@@ -75,12 +95,12 @@ const sketch = {
                             : palette.colors[0],
                         anchor: 'middle'
                     })
+                    w++
                 }
                 const textWidth = (SVG.elem.lastChild.getBoundingClientRect().width/svgDisplayWidth) * SVG.width 
                 x += textWidth + fontSize * .5
-                w++
             }
-            fontSize *= l % 7 === 0 ? 1.33 :  0.66
+            fontSize *= l % 4 === 0 ? 1.16 :  0.66
             
             if (l % 9 === 0) { 
                 fontSize = base
