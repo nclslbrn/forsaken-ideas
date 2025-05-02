9ceddc219c34116637f6139fe851aa6205dd45b6
nclslbrn <nclslbrn@gmail.com>
2025-05-02 16:32:12 +0200

    add line scanning to convert GSLS comp into SVG

--- sketch/2025-04-30/index.js
+++ sketch/2025-04-30/index.js
@@ -3,15 +3,28 @@ import infobox from '../../sketch-common/infobox'
 import handleAction from '../../sketch-common/handle-action'
 import vertSrc from './glsl/base.vert'
 import fragSrc from './glsl/base.frag'
-import { canvasRecorder } from '@thi.ng/dl-asset'
 
-let isRecording = false,
-    recorder = false,
-    traits = {}
+import SvgTracer from '../../sketch-common/svg-tracer'
+import { fillWithStraightLines } from '../../sketch-common/fillShape'
+
+let traits = {}
 
 const { floor, ceil, random } = Math
-
 const shuffle = (array) => array.sort(() => 0.5 - random())
+
+const containerElement = document.getElementById('windowFrame'),
+    loader = document.getElementById('loading'),
+    canvas = document.createElement('canvas'),
+    gl = canvas.getContext('webgl', { preserveDrawingBuffer: true }),
+    dpi = 300,
+    svg = new SvgTracer({
+        parentElem: containerElement,
+        size: 'A3_square',
+        background: 'white',
+        dpi
+    }),
+    S = [2560, 2560],
+    margin = svg.cmToPixels(1)
 
 const splitCell = (cellIdx, isHorizontal, grid) => {
     if (grid[cellIdx] === undefined) return grid
@@ -42,28 +55,6 @@ const capture = (canvas) => {
     link.click()
 }
 
-const startRecording = (canvas) => {
-    if (isRecording) return
-    recorder = canvasRecorder(
-        canvas,
-        'geometric-art',
-        {
-            mimeType: 'video/webm;codecs=vp8',
-            fps: 30
-        }
-    )
-    recorder.start()
-    console.log('%c Record started ', 'background: tomato; color: white')
-    isRecording = true
-}
-
-const stopRecording = () => {
-    if (!isRecording) return
-    recorder.stop()
-    console.log('%c Record stopped ', 'background: limegreen; color: black')
-    isRecording = false
-}
-
 const createShader = (gl, type, source) => {
     const shader = gl.createShader(type)
     gl.shaderSource(shader, source)
@@ -89,9 +80,8 @@ const createProgram = (gl, vertexShader, fragmentShader) => {
 }
 
 const sketch = {
-    frame: 0,
     init: () => {
-        const numCell = 6 + ceil(random() * 24)
+        const numCell = 2 + ceil(random() * 6)
         let cells = [[0.5, 0.5, 1, 1]]
 
         for (let i = 0; i < numCell; i++)
@@ -108,6 +98,7 @@ const sketch = {
             cells
         }
         sketch.render()
+        sketch.renderSvg()
     },
     setup: () => {
         if (!gl) {
@@ -115,8 +106,8 @@ const sketch = {
             return
         }
 
-        canvas.width = 2400
-        canvas.height = 2400
+        canvas.width = S[0]
+        canvas.height = S[1]
         gl.viewport(0, 0, canvas.width, canvas.height)
         // Create shaders and program
         const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertSrc),
@@ -129,11 +120,11 @@ const sketch = {
             verts = new Float32Array([-1, -1, 1, -1, -1, 1, 1, 1]),
             positionLoc = gl.getAttribLocation(program, 'a_position')
 
-        sketch.uTimeLoc = gl.getUniformLocation(program, 'u_time'),
-        sketch.uResolution = gl.getUniformLocation(program, 'u_resolution'),
-        sketch.uNoiseSeed = gl.getUniformLocation(program, 'u_noiseSeed'),
-        sketch.uNoiseSize = gl.getUniformLocation(program, 'u_noiseSize'),
-        sketch.uNumCell = gl.getUniformLocation(program, 'u_numCell'),
+        sketch.uTimeLoc = gl.getUniformLocation(program, 'u_time')
+        sketch.uResolution = gl.getUniformLocation(program, 'u_resolution')
+        sketch.uNoiseSeed = gl.getUniformLocation(program, 'u_noiseSeed')
+        sketch.uNoiseSize = gl.getUniformLocation(program, 'u_noiseSize')
+        sketch.uNumCell = gl.getUniformLocation(program, 'u_numCell')
         sketch.uCells = gl.getUniformLocation(program, 'u_cells')
 
         gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
@@ -142,14 +133,11 @@ const sketch = {
         gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0)
         gl.disable(gl.DEPTH_TEST)
         gl.enable(gl.BLEND)
-        sketch.init()
-        //startRecording(canvas)
 
         gl.clearColor(1, 1, 1, 1)
-        sketch.render()
     },
+
     render: () => {
-        //frame === 100 && stopRecording()
         gl.clear(gl.COLOR_BUFFER_BIT)
         gl.uniform1f(sketch.uTimeLoc, sketch.frame * 0.01)
         gl.uniform2f(sketch.uResolution, canvas.width, canvas.height)
@@ -161,27 +149,56 @@ const sketch = {
             traits.cells.reduce((s, c) => [...s, ...c], [])
         )
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
+    },
 
-        sketch.frame++
-        //requestAnimationFrame(sketch.render)
+    renderSvg: () => {
+        svg.clear()
+        // Four gray values 51, 102, 153, 204,  
+        const scanLines = [
+            ...fillWithStraightLines(canvas, (c) => c <= 51, 8, 0),
+            ...fillWithStraightLines(canvas, (c) => c > 51 && c <= 102, 16, 1),
+            ...fillWithStraightLines(canvas, (c) => c > 102 && c <= 153, 32, 2),
+            ...fillWithStraightLines(canvas, (c) => c > 153 && c <= 204, 64, 3),
+            ...fillWithStraightLines(canvas, (c) => c > 204, 128, 0)
+        ]
+        
+        scanLines.forEach((line) => {
+            svg.path({
+                points: line.map((v) => [
+                    margin + (v[0] / canvas.width) * (svg.width - margin * 2),
+                    margin + (v[1] / canvas.height) * (svg.height - margin * 2)
+                ]),
+                stroke: '#333',
+                strokeWidth: 4,
+                fill: 'none',
+                close: false,
+                strokeLinecap: 'round'
+            })
+        })
     }
 }
-
-const containerElement = document.getElementById('windowFrame'),
-    loader = document.getElementById('loading'),
-    canvas = document.createElement('canvas'),
-    gl = canvas.getContext('webgl', { preserveDrawingBuffer: true })
+containerElement.style.gridTemplateRows = '1fr 48vh 48vh 1fr'
 
 containerElement.removeChild(loader)
 containerElement.appendChild(canvas)
+svg.init()
+svg.elem.style.gridRowStart = '3'
 sketch.setup()
-window.infobox = infobox
-window.randomize = sketch.init
-window.capture = () => capture(canvas)
+sketch.init()
 
+window['infobox'] = infobox
+window['randomize'] = sketch.init
+window['capture'] = () => capture(canvas)
+window['downloadSVG'] = () =>
+    svg.export({
+        name: `2025-04-30_Nicolas_Lebrun__${new Date().toISOString()}`
+    })
+window['downloadPNG'] = () =>
+    svg.exportPng({
+        name: `2025-04-30_Nicolas_Lebrun__${new Date().toISOString()}`,
+        quality: 1
+    })
 window.onkeydown = (e) => {
-    if (e.key.toLowerCase() === 'r') startRecording(canvas)
-    if (e.key.toLowerCase() === 's') stopRecording()
     if (e.key.toLowerCase() === 'g') init()
 }
 
