0b45d7cdd899fdb66d417198414df929c06253a7
Nicolas Lebrun <nclslbrn@gmail.com>
2025-09-26 17:12:24 +0200

    make evolving diagonal line spacing

--- sketch/grid-rules/fillCell.js
+++ sketch/grid-rules/fillCell.js
@@ -1,22 +1,21 @@
-const {sin, cos, hypot, PI} = Math
+const { sin, cos, hypot, PI } = Math
 
 const fillCell = (cell, dir, decay) => {
-    
-    if (dir>2) console.error(`${dir}/2`)
-    
+    if (dir > 2) console.error(`${dir}/2`)
+
     const [cx, cy, cw, ch] = cell
-    const isValidPos = (x, y) => x > cx && x < cx+cw && y > cy && y < cy+ch  
+    const isValidPos = (x, y) => x > cx && x < cx + cw && y > cy && y < cy + ch
     const ls = []
-    
-    let res = 0.5 
+
+    let res = 0.25
 
     if (dir < 2) {
         // vertical lines
         if (dir === 0) {
             res = 1
-            for (let x = cx-decay; x < cx+cw; x += res) {
+            for (let x = cx - decay; x < cx + cw; x += res) {
                 let ln = []
-                for (let y = cy; y < cy+ch; y++) {
+                for (let y = cy; y < cy + ch; y++) {
                     const penDown = isValidPos(x, y)
                     if (ln.length && !penDown) {
                         ls.push(ln)
@@ -31,9 +30,9 @@ const fillCell = (cell, dir, decay) => {
         // horizontal lines
         else {
             res = 1
-            for (let y = cy-decay; y < cy+ch; y += res) {
+            for (let y = cy - decay; y < cy + ch; y += res) {
                 let ln = []
-                for (let x = cx; x < cx+cw; x++) {
+                for (let x = cx; x < cx + cw; x++) {
                     const penDown = isValidPos(x, y)
                     if (ln.length && !penDown) {
                         ls.push(ln)
@@ -51,23 +50,27 @@ const fillCell = (cell, dir, decay) => {
         const diag = hypot(cw, ch)
         const theta = (dir === 2 ? -PI : PI) / 4
         const cntr = [cx + cw / 2, cy + ch / 2]
-
-        res = 0.0001
-        
-        for (let x = cx-diag; x <= cx+diag; x += res) {
-                   
+        // res /= 10000
+        res = 0.5
+        for (let x = cx - diag; x <= cx + diag; x += res) {
             let ln = []
-            for (let y = cy-diag; y <= cy+diag; y++) {
-                const xx = cos(theta) * (x - cntr[0]) - sin(theta) * (y - cntr[1]) + cntr[0]
-                const yy = sin(theta) * (x - cntr[0]) + cos(theta) * (y - cntr[1]) + cntr[1]
+            for (let y = cy - diag; y <= cy + diag; y++) {
+                const xx =
+                    cos(theta) * (x - cntr[0]) -
+                    sin(theta) * (y - cntr[1]) +
+                    cntr[0]
+                const yy =
+                    sin(theta) * (x - cntr[0]) +
+                    cos(theta) * (y - cntr[1]) +
+                    cntr[1]
                 const penDown = isValidPos(xx, yy)
                 if (ln.length && !penDown) {
                     ls.push(ln)
                     ln = []
                 }
                 if (penDown) ln.push([xx, yy])
-                res += 0.0005
             }
+            res *= 1.05
             ln.length && ls.push(ln)
         }
     }
--- sketch/grid-rules/index.js
+++ sketch/grid-rules/index.js
@@ -13,16 +13,16 @@ import GRIDS from './GRIDS'
 import { fillCell } from './fillCell'
 
 const DPI = quantity(96, dpi),
-    CUSTOM_FORMAT = quantity([216, 270], "mm"),
+    CUSTOM_FORMAT = quantity([216, 270], 'mm'),
     SIZE = mul(CUSTOM_FORMAT, DPI).deref(),
     MARGIN = convert(mul(quantity(15, mm), DPI), NONE),
     ROOT = document.getElementById('windowFrame'),
     CANVAS = document.createElement('canvas'),
     CTX = CANVAS.getContext('2d')
 
-const remap = (n, start1, stop1, start2, stop2) => (n - start1) / (stop1 - start1) * (stop2 - start2) + start2,
+const remap = (n, start1, stop1, start2, stop2) =>
+        ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2,
     { random, floor, ceil } = Math
-
 
 let drawElems
 
@@ -37,53 +37,52 @@ const init = () => {
             .map((_, i) => i)
             .filter((idx) => !not.includes(idx))
 
-        const gridTypeIdx = floor(random() * choices.length);
+        const gridTypeIdx = floor(random() * choices.length)
 
         return [
             choices[gridTypeIdx],
             GRIDS[choices[gridTypeIdx]](numCell, rand)
-        ];
-    };
+        ]
+    }
 
-    const rule = pickRandom(RULES);
-    const grid_size = [2 + ceil(random() * 4), 2 + ceil(random() * 12)]
+    const rule = pickRandom(RULES)
+    const grid_size = [6 + ceil(random() * 4), 8 + ceil(random() * 12)]
     const [patternType, pattern] = cells(grid_size[0], random)
     const [_, grid] = cells(grid_size[1], random, [patternType])
     const allCell = grid.reduce((rects, cell, i) => {
         const [x, y, w, h] = cell
-        return [...rects, ...pattern.reduce((subgrid, pattern, j) => {
-            if (!rule(i, j)) {
-                const [dx, dy, dw, dh] = pattern
-                return [
-                    ...subgrid,
-                    [
-                        remap(x + (dx * w), 0, 1, MARGIN, SIZE[0] - MARGIN),
-                        remap(y + (dy * h), 0, 1, MARGIN, SIZE[1] - MARGIN),
-                        dw * w * (SIZE[0] - MARGIN * 2),
-                        dh * h * (SIZE[1] - MARGIN * 2)
+        return [
+            ...rects,
+            ...pattern.reduce((subgrid, pattern, j) => {
+                if (!rule(i, j)) {
+                    const [dx, dy, dw, dh] = pattern
+                    return [
+                        ...subgrid,
+                        [
+                            remap(x + dx * w, 0, 1, MARGIN, SIZE[0] - MARGIN),
+                            remap(y + dy * h, 0, 1, MARGIN, SIZE[1] - MARGIN),
+                            dw * w * (SIZE[0] - MARGIN * 2),
+                            dh * h * (SIZE[1] - MARGIN * 2)
+                        ]
                     ]
-                ]
-            } else {
-                return subgrid
-            }
-        },
-            [])
+                } else {
+                    return subgrid
+                }
+            }, [])
         ]
     }, [])
-    
-    const lines = allCell.reduce((acc, cell) => [
-            ...acc,
-            ...fillCell(
-                cell, 
-                floor(random()*3), 
-                floor(random()*2)*4
-            ).map(ln => polyline(ln))
-    ], [])
 
-    drawElems = [
-        rect(SIZE, { fill: "#fff" }),
-        group({stroke: '#000'}, lines)
-    ]
+    const lines = allCell.reduce(
+        (acc, cell) => [
+            ...acc,
+            ...fillCell(cell, floor(random() * 3), floor(random() * 2) * 4).map(
+                (ln) => polyline(ln)
+            )
+        ],
+        []
+    )
+
+    drawElems = [rect(SIZE, { fill: '#fff' }), group({ stroke: '#000' }, lines)]
     draw(CTX, group({}, drawElems))
 }
 
@@ -98,7 +97,11 @@ window['exportSVG'] = () => {
         `Grid rules-${FMT_yyyyMMdd_HHmmss()}.svg`,
         asSvg(
             svgDoc(
-                { width: SIZE[0], height: SIZE[1], viewBox: `0 0 ${SIZE[0]} ${SIZE[1]}` },
+                {
+                    width: SIZE[0],
+                    height: SIZE[1],
+                    viewBox: `0 0 ${SIZE[0]} ${SIZE[1]}`
+                },
                 group({}, drawElems)
             )
         )
