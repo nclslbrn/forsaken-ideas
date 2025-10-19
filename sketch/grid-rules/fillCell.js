9ec427d1456ce34b6a87df964e0b4ea38931eda4
Nicolas Lebrun <nclslbrn@gmail.com>
2025-10-19 18:20:38 +0200

    add new rules + full viewport rendering

--- sketch/grid-rules/fillCell.js
+++ sketch/grid-rules/fillCell.js
@@ -1,80 +1,133 @@
-const { sin, cos, hypot, PI } = Math
+const { sin, cos, hypot, sqrt, pow, max, PI } = Math
+
+const verticalLines = ([cx, cy, cw, ch], decay, isValidPos, res) => {
+    const ls = []
+    for (let x = cx - decay; x < cx + cw; x += res) {
+        let ln = []
+        for (let y = cy; y < cy + ch; y++) {
+            const penDown = isValidPos(x, y)
+            if (ln.length && !penDown) {
+                ls.push(ln)
+                ln = []
+            }
+            if (penDown) ln.push([x, y])
+        }
+        ln.length && ls.push(ln)
+        res *= 1.07
+    }
+    return ls
+}
+
+const horizontalLines = ([cx, cy, cw, ch], decay, isValidPos, res) => {
+    const ls = []
+    for (let y = cy - decay; y < cy + ch; y += res) {
+        let ln = []
+        for (let x = cx; x < cx + cw; x++) {
+            const penDown = isValidPos(x, y)
+            if (ln.length && !penDown) {
+                ls.push(ln)
+                ln = []
+            }
+            if (penDown) ln.push([x, y])
+        }
+        ln.length && ls.push(ln)
+        res *= 1.07
+    }
+    return ls
+}
+
+const diagonalLines = ([cx, cy], isValidPos, res, theta, cntr, diag) => {
+    const ls = []
+    for (let x = cx - diag; x <= cx + diag; x += res) {
+        let ln = []
+        for (let y = cy - diag; y <= cy + diag; y++) {
+            const xx =
+                cos(theta) * (x - cntr[0]) -
+                sin(theta) * (y - cntr[1]) +
+                cntr[0]
+            const yy =
+                sin(theta) * (x - cntr[0]) +
+                cos(theta) * (y - cntr[1]) +
+                cntr[1]
+            const penDown = isValidPos(xx, yy)
+            if (ln.length && !penDown) {
+                ls.push(ln)
+                ln = []
+            }
+            if (penDown) ln.push([xx, yy])
+        }
+        res *= 1.03
+        ln.length && ls.push(ln)
+    }
+    return ls
+}
+
+const arcLines = (
+    [cx, cy, cw, ch],
+    decay,
+    isValidPos,
+    res,
+    [x, y],
+    [start, end]
+) => {
+    const s = max(cw, ch)
+    const ls = []
+    for (let d = decay; d <= sqrt(pow(s, 2) * 2); d += res) {
+        const ln = []
+        for (let l = start; l <= end; l += 0.01) {
+            const ax = x + cos(l) * d,
+                ay = y + sin(l) * d
+            isValidPos(ax, ay) && ln.push([ax, ay])
+        }
+        ln.length && ls.push(ln)
+        res *= 1.03
+    }
+    return ls
+}
 
 const fillCell = (cell, dir, decay) => {
-    if (dir > 2) console.error(`${dir}/2`)
+    if (dir > 7) console.error(`${dir}/7`)
 
     const [cx, cy, cw, ch] = cell
     const isValidPos = (x, y) => x > cx && x < cx + cw && y > cy && y < cy + ch
-    const ls = []
 
-    let res = 0.25
-
-    if (dir < 2) {
-        // vertical lines
-        if (dir === 0) {
-            res = 1
-            for (let x = cx - decay; x < cx + cw; x += res) {
-                let ln = []
-                for (let y = cy; y < cy + ch; y++) {
-                    const penDown = isValidPos(x, y)
-                    if (ln.length && !penDown) {
-                        ls.push(ln)
-                        ln = []
-                    }
-                    if (penDown) ln.push([x, y])
-                }
-                ln.length && ls.push(ln)
-                res *= 1.07
-            }
-        }
-        // horizontal lines
-        else {
-            res = 1
-            for (let y = cy - decay; y < cy + ch; y += res) {
-                let ln = []
-                for (let x = cx; x < cx + cw; x++) {
-                    const penDown = isValidPos(x, y)
-                    if (ln.length && !penDown) {
-                        ls.push(ln)
-                        ln = []
-                    }
-                    if (penDown) ln.push([x, y])
-                }
-                ln.length && ls.push(ln)
-                res *= 1.07
-            }
-        }
+    switch (dir) {
+        case 0:
+            return verticalLines(cell, decay, isValidPos, 1)
+        case 1:
+            return horizontalLines(cell, decay, isValidPos, 1)
+        case 2:
+        case 3:
+            const diag = hypot(cw, ch)
+            const theta = (dir === 2 ? -PI : PI) / 4
+            const cntr = [cx + cw / 2, cy + ch / 2]
+            return diagonalLines(cell, isValidPos, 1e-8, theta, cntr, diag)
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+            const cntrs = [
+                [cx, cy], // left to top
+                [cx + cw, cy], // top to right
+                [cx + cw, cy + ch], // right to bottom
+                [(cx, cy + ch)] // bottom to left
+            ]
+            const startEnds = [
+                // same order here
+                [0, PI / 2],
+                [PI / 2, PI],
+                [PI, PI * 1.5],
+                [PI * 1.5, PI * 2]
+            ]
+            return arcLines(
+                cell,
+                decay,
+                isValidPos,
+                2,
+                cntrs[dir % 4],
+                startEnds[dir % 4]
+            )
     }
-    // diagonal lines
-    else {
-        const diag = hypot(cw, ch)
-        const theta = (dir === 2 ? -PI : PI) / 4
-        const cntr = [cx + cw / 2, cy + ch / 2]
-        // res /= 10000
-        res = 1e-8
-        for (let x = cx - diag; x <= cx + diag; x += res) {
-            let ln = []
-            for (let y = cy - diag; y <= cy + diag; y++) {
-                const xx =
-                    cos(theta) * (x - cntr[0]) -
-                    sin(theta) * (y - cntr[1]) +
-                    cntr[0]
-                const yy =
-                    sin(theta) * (x - cntr[0]) +
-                    cos(theta) * (y - cntr[1]) +
-                    cntr[1]
-                const penDown = isValidPos(xx, yy)
-                if (ln.length && !penDown) {
-                    ls.push(ln)
-                    ln = []
-                }
-                if (penDown) ln.push([xx, yy])
-            }
-            res *= 1.03
-            ln.length && ls.push(ln)
-        }
-    }
-    return ls
 }
 
 export { fillCell }
--- sketch/grid-rules/index.js
+++ sketch/grid-rules/index.js
@@ -1,7 +1,7 @@
 import { rect, polyline, group, svgDoc, asSvg } from '@thi.ng/geom'
 import { pickRandom } from '@thi.ng/random'
 import { FMT_yyyyMMdd_HHmmss } from '@thi.ng/date'
-import '../framed-canvas.css'
+import '../full-canvas.css'
 import infobox from '../../sketch-common/infobox'
 import handleAction from '../../sketch-common/handle-action'
 import { downloadCanvas, downloadWithMime } from '@thi.ng/dl-asset'
@@ -14,14 +14,16 @@ import GRIDS from './GRIDS'
 import { fillCell } from './fillCell'
 
 const DPI = quantity(96, dpi),
-    CUSTOM_FORMAT = quantity([216, 270], 'mm'),
+    CUSTOM_FORMAT = quantity(
+        [window.innerWidth / 50, window.innerHeight / 50],
+        'cm'
+    ),
     SIZE = mul(CUSTOM_FORMAT, DPI).deref(),
     MARGIN = convert(mul(quantity(20, mm), DPI), NONE),
     ROOT = document.getElementById('windowFrame'),
     CANVAS = document.createElement('canvas'),
     CTX = CANVAS.getContext('2d'),
-    CHARS = [...'WHO\'S THE PM TODAY ?']
-
+    CHARS = [..."WHO'S THE PM TODAY ?"]
 
 const remap = (n, start1, stop1, start2, stop2) =>
         ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2,
@@ -41,46 +43,60 @@ const init = () => {
             .filter((idx) => !not.includes(idx))
 
         const gridTypeIdx = floor(random() * choices.length)
-
         return [
             choices[gridTypeIdx],
             GRIDS[choices[gridTypeIdx]](numCell, rand)
         ]
     }
 
+    const useText = Math.random() > 0.5
     const rule = pickRandom(RULES)
     const grid_size = [6 + ceil(random() * 4), 8 + ceil(random() * 3)]
     const [patternType, pattern] = cells(grid_size[0], random)
     const [_, grid] = cells(grid_size[1], random, [patternType])
-    const allCell = grid.reduce((rects, cell, i) => {
-        const [x, y, w, h] = cell
-        const nested = pattern.reduce((subgrid, pattern, j) => {
-            const [dx, dy, dw, dh] = pattern
-            const patternCell = [
-                remap(x + dx * w, 0, 1, MARGIN, SIZE[0] - MARGIN),
-                remap(y + dy * h, 0, 1, MARGIN, SIZE[1] - MARGIN),
-                dw * w * (SIZE[0] - MARGIN * 2),
-                dh * h * (SIZE[1] - MARGIN * 2)
+    const allCell = grid.reduce(
+        (rects, cell, i) => {
+            const [x, y, w, h] = cell
+            const nested = pattern.reduce(
+                (subgrid, pattern, j) => {
+                    const [dx, dy, dw, dh] = pattern
+                    const patternCell = [
+                        remap(x + dx * w, 0, 1, MARGIN, SIZE[0] - MARGIN),
+                        remap(y + dy * h, 0, 1, MARGIN, SIZE[1] - MARGIN),
+                        dw * w * (SIZE[0] - MARGIN * 2),
+                        dh * h * (SIZE[1] - MARGIN * 2)
+                    ]
+                    if (!rule(i, j)) {
+                        return [[...subgrid[0], patternCell], subgrid[1]]
+                    } else {
+                        return [
+                            subgrid[0],
+                            [...subgrid[1], useText ? patternCell : []]
+                        ]
+                    }
+                },
+                [[], []]
+            )
+            return [
+                [...rects[0], ...nested[0]],
+                [...rects[1], ...nested[1]]
             ]
-            if (!rule(i, j)) {
-                 return [[...subgrid[0], patternCell], subgrid[1]]
-            } else {
-                return [subgrid[0], [...subgrid[1], patternCell]]
-            }
-            }, [[], []]
-        )
-        return [[...rects[0], ...nested[0]], [...rects[1], ...nested[1]]]
-    }, [[], []])
+        },
+        [[], []]
+    )
 
     const lines = [
         ...allCell[0].reduce(
-        (acc, cell) => [
-            ...acc,
-            ...fillCell(cell, floor(random() * 3), floor(random() * 2) * 4).map(
-                (ln) => polyline(ln)
-            )
-        ],
-        []),
+            (acc, cell) => [
+                ...acc,
+                ...fillCell(
+                    cell,
+                    floor(random() * 8),
+                    floor(random() * 2) * 4
+                ).map((ln) => polyline(ln))
+            ],
+            []
+        ),
         ...allCell[1].reduce(
             (acc, cell, cellIdx) => [
                 ...acc,
@@ -89,14 +105,16 @@ const init = () => {
                     CHARS[cellIdx % CHARS.length],
                     [cell[2], cell[3]],
                     [cell[0], cell[1]]
-                ).map(
-                    ln => polyline(ln)
-                )
+                ).map((ln) => polyline(ln))
             ],
-        [])
+            []
+        )
     ]
 
-    drawElems = [rect(SIZE, { fill: '#111' }), group({ stroke: '#fefefe' }, lines)]
+    drawElems = [
+        rect(SIZE, { fill: '#111' }),
+        group({ stroke: '#fefefe' }, lines)
+    ]
     draw(CTX, group({}, drawElems))
 }
 
@@ -121,6 +139,18 @@ window['exportSVG'] = () => {
         )
     )
 }
-
+document.addEventListener('keypress', (e) => {
+    switch (e.key) {
+        case 'r':
+            window.init()
+            break
+        case 'j':
+            window.exportJPG()
+            break
+        case 's':
+            window.exportSVG()
+            break
+    }
+})
 window.infobox = infobox
 handleAction()
