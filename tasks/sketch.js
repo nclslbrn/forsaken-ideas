b3fb1e0beb217553d78141839776efb29f59e9d2
Nicolas Lebrun <nclslbrn@gmail.com>
2025-10-24 16:15:48 +0200

    remove node spawn args (will be deprecated)

--- tasks/sketch.js
+++ tasks/sketch.js
@@ -7,24 +7,16 @@ const command = process.argv[3] || 'dev';
 
 
 if (!folderName) {
-  console.error('Usage: node scripts/sketch.js <folder-name> [dev|build]');
+  console.error('Missing sketch folder name. Usage: [npm|yarn] run sketch <folder-name> [dev|build]');
   process.exit(1);
 }
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const configPath = path.join(
-  __dirname, '../', 'sketch', folderName //, 'vite.config.js'
-);
-const viteCommand = command === 'build' ? 'build' : 'dev';
 
-console.log(configPath)
-
-const args = ['vite', viteCommand];
-const child = spawn('npx', args, {
+const child = spawn(`npx vite ${command === 'build' ? 'build' : 'dev'}`,{
   stdio: 'inherit',
   shell: true,
-  // cwd: process.cwd(),
-  cwd: configPath
+  cwd: path.join(__dirname, '../', 'sketch', folderName)
 });
 
 child.on('exit', (code) => {
